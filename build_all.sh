9fd52abddbc8369e8c10603d9b2707c485b3c68a
Alex Duchesne <alex@netvps.ca>
2020-03-23 12:13:15 -0400

    Tweaked build config of all emulators to get the wifi stack to compile correctly

--- build_all.sh
+++ build_all.sh
@@ -4,18 +4,21 @@ echo "Building the interface"
 cd retro-go
 make -j 6 || exit
 
+echo ""
 echo "Building the NES emulator"
 cd ../nesemu-go
 make -j 6 || exit
 
+echo ""
 echo "Building the Gameboy emulator"
 cd ../gnuboy-go
 make -j 6 || exit
 
+echo ""
 echo "Building the SMS/Gamegear/Coleco emulator"
 cd ../smsplusgx-go
 make -j 6 || exit
 
-cd ..
-
+echo ""
 echo "All done!"
+cd ..
--- mkfw.sh
+++ mkfw.sh
@@ -7,9 +7,9 @@ mkfw="../odroid-go-multi-firmware/tools/mkfw/mkfw"
 release=`date +%Y%m%d`;
 
 $mkfw "Retro-Go ($release)" assets/tile.raw \
-	0 16 655360 frontend retro-go/build/retro-go.bin  \
-	0 17 524288 nesemu nesemu-go/build/nesemu-go.bin  \
-	0 18 458752 gnuboy gnuboy-go/build/gnuboy-go.bin  \
-	0 19 917504 smsplusgx smsplusgx-go/build/smsplusgx-go.bin
+	0 16 655360  frontend retro-go/build/retro-go.bin  \
+	0 17 851968  nesemu nesemu-go/build/nesemu-go.bin  \
+	0 18 851968  gnuboy gnuboy-go/build/gnuboy-go.bin  \
+	0 19 1310720 smsplusgx smsplusgx-go/build/smsplusgx-go.bin
 
 mv firmware.fw retro-go_$release.fw
