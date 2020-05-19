ducalex/retro-go

commit cc97d3c1fdeddf9380558031347aa0d9fbdae572

NES: New MMC5 implementation (A few games work but still incomplete)

nesemu-go/components/nofrendo/cpu/nes6502.c
@@ -23,7 +23,6 @@
 ** $Id: nes6502.c,v 1.2 2001/04/27 14:37:11 neil Exp $
 */
 
-
 #include <noftypes.h>
 #include <nofrendo.h>
 #include <esp_attr.h>
@@ -34,16 +33,12 @@
 static nes6502_t cpu;
 static int remaining_cycles = 0;
 
-/* memory region pointers */
-static uint8 *ram = NULL, *stack = NULL;
-static uint8 null_page[NES6502_BANKSIZE];
-
 
 // #define NES6502_TESTOPS
 #define  NES6502_JUMPTABLE
 
 
-#define  ADD_CYCLES(x) \
+#define ADD_CYCLES(x) \
 { \
    remaining_cycles -= (x); \
    cpu.total_cycles += (x); \
@@ -67,13 +62,13 @@ static uint8 null_page[NES6502_BANKSIZE];
 /* Immediate */
 #define IMMEDIATE_BYTE(value) \
 { \
-   value = bank_readbyte(PC++); \
+   value = fast_readbyte(PC++); \
 }
 
 /* Absolute */
 #define ABSOLUTE_ADDR(address) \
 { \
-   address = bank_readword(PC); \
+   address = fast_readword(PC); \
    PC += 2; \
 }
 
@@ -237,8 +232,8 @@ static uint8 null_page[NES6502_BANKSIZE];
 
 
 /* Stack push/pull */
-#define  PUSH(value)             stack[S--] = (uint8) (value)
-#define  PULL()                  stack[++S]
+#define PUSH(value)             cpu.ram[STACK_OFFSET + S--] = (uint8) (value)
+#define PULL()                  cpu.ram[STACK_OFFSET + ++S]
 
 
 /*
@@ -254,7 +249,7 @@ static uint8 null_page[NES6502_BANKSIZE];
 */
 
 /* Scatter flags to separate variables */
-#define  SCATTER_FLAGS(value) \
+#define SCATTER_FLAGS(value) \
 { \
    n_flag = (value) & N_FLAG; \
    v_flag = (value) & V_FLAG; \
@@ -266,7 +261,7 @@ static uint8 null_page[NES6502_BANKSIZE];
 }
 
 /* Combine flags into flag register */
-#define  COMBINE_FLAGS() \
+#define COMBINE_FLAGS() \
 ( \
    (n_flag & N_FLAG) \
    | (v_flag ? V_FLAG : 0) \
@@ -279,7 +274,7 @@ static uint8 null_page[NES6502_BANKSIZE];
 )
 
 /* Set N and Z flags based on given value */
-#define  SET_NZ_FLAGS(value)     n_flag = z_flag = (value);
+#define SET_NZ_FLAGS(value)     n_flag = z_flag = (value);
 
 /* For BCC, BCS, BEQ, BMI, BNE, BPL, BVC, BVS */
 #define RELATIVE_BRANCH(condition) \
@@ -301,7 +296,7 @@ static uint8 null_page[NES6502_BANKSIZE];
 
 #define JUMP(address) \
 { \
-   PC = bank_readword((address)); \
+   PC = fast_readword((address)); \
 }
 
 /*
@@ -632,10 +627,10 @@ static uint8 null_page[NES6502_BANKSIZE];
 
 #define JMP_INDIRECT() \
 { \
-   temp = bank_readword(PC); \
+   temp = fast_readword(PC); \
    /* bug in crossing page boundaries */ \
    if (0xFF == (temp & 0xFF)) \
-      PC = (bank_readbyte(temp & 0xFF00) << 8) | bank_readbyte(temp); \
+      PC = (fast_readbyte(temp & 0xFF00) << 8) | fast_readbyte(temp); \
    else \
       JUMP(temp); \
    ADD_CYCLES(5); \
@@ -1029,13 +1024,13 @@ static uint8 null_page[NES6502_BANKSIZE];
    ADD_CYCLES(2); \
 }
 
-#define  DECLARE_LOCAL_REGS() \
+#define DECLARE_LOCAL_REGS() \
    uint32 PC; \
    uint8 A, X, Y, S; \
    uint8 n_flag, v_flag, b_flag; \
    uint8 d_flag, i_flag, z_flag, c_flag;
 
-#define  GET_GLOBAL_REGS() \
+#define GET_GLOBAL_REGS() \
 { \
    PC = cpu.pc_reg; \
    A = cpu.a_reg; \
@@ -1045,7 +1040,7 @@ static uint8 null_page[NES6502_BANKSIZE];
    S = cpu.s_reg; \
 }
 
-#define  STORE_LOCAL_REGS() \
+#define STORE_LOCAL_REGS() \
 { \
    cpu.pc_reg = PC; \
    cpu.a_reg = A; \
@@ -1059,152 +1054,138 @@ static uint8 null_page[NES6502_BANKSIZE];
 ** Zero-page helper macros
 */
 
-#define  ZP_READBYTE(addr)          (ram[(addr)])
-#define  ZP_WRITEBYTE(addr, value)  (ram[(addr)] = (uint8) (value))
-#define  ZP_READWORD(addr)          (*(uint16 *)(ram + (addr)))
+#define ZP_READBYTE(addr)          (cpu.ram[(addr)])
+#define ZP_WRITEBYTE(addr, value)  (cpu.ram[(addr)] = (uint8) (value))
 
-#ifndef HOST_LITTLE_ENDIAN
-#undef   ZP_READWORD
-#define  ZP_READWORD(addr) bank_readword(addr)
-#endif
-
-INLINE uint8 bank_readbyte(uint32 address)
-{
-   return cpu.mem_page[(address) >> NES6502_BANKSHIFT][(address) & NES6502_BANKMASK];
-}
-
-INLINE void bank_writebyte(uint32 address, uint8 value)
-{
-   cpu.mem_page[(address) >> NES6502_BANKSHIFT][(address) & NES6502_BANKMASK] = value;
-}
-
-INLINE uint32 bank_readword(register uint32 address)
-{
-   /* technically, this should fail if the address is $xFFF, but
-   ** any code that does this would be suspect anyway, as it would
-   ** be fetching a word across page boundaries, which only would
-   ** make sense if the banks were physically consecutive.
-   */
 #ifdef HOST_LITTLE_ENDIAN
-   return (uint32) (*(uint16 *)(cpu.mem_page[address >> NES6502_BANKSHIFT] + (address & NES6502_BANKMASK)));
+#define ZP_READWORD(addr)          (*(uint16 *)(cpu.ram + (addr)))
 #else
-   uint32 x = (uint32) *(uint16 *)(cpu.mem_page[address >> NES6502_BANKSHIFT] + (address & NES6502_BANKMASK));
-   return (x << 8) | (x >> 8);
+#define ZP_READWORD(addr)          fast_readword(addr)
 #endif
+
+/* read a byte from mapped memory, ignoring unmapped/IO areas (for speed) */
+INLINE uint8 fast_readbyte(uint32 address)
+{
+   uint8 *page = cpu.mem_pages[address >> NES6502_BANKSHIFT];
+   return (page == NULL) ? 0xFF : page[address & NES6502_BANKMASK];
 }
 
-/* read a byte of 6502 memory */
+/* read a word from mapped memory, ignoring unmapped/IO areas (for speed) */
+INLINE uint32 fast_readword(uint32 address)
+{
+   return fast_readbyte(address + 1) << 8 | fast_readbyte(address);
+}
+
+/* read a byte of 6502 memory space */
 INLINE uint8 mem_readbyte(uint32 address)
 {
-   nes6502_memread *mr;
+   uint8 *page = cpu.mem_pages[address >> NES6502_BANKSHIFT];
 
-   /* TODO: following 2 cases are N2A03-specific */
+   /* RAM */
    if (address < 0x2000)
    {
-      /* RAM */
-      return ram[address & 0x7FF];
+      return page[address & 0x7FF];
    }
-   else if (address < 0x8000)
+
+   /* Special memory handlers */
+   if (address < 0x8000) // Assume anything beyond 0x8000 is mapped below
    {
-      /* check memory range handlers */
-      for (mr = cpu.read_handlers; mr->min_range != NULL; mr++)
+      for (nes6502_memread *mr = cpu.read_handlers; mr->read_func != NULL; mr++)
       {
          if (address >= mr->min_range && address <= mr->max_range)
             return mr->read_func(address);
       }
    }
 
-   /* return paged memory */
-   return cpu.mem_page[address >> NES6502_BANKSHIFT][address & NES6502_BANKMASK];
+   /* Unmapped region */
+   if (page == NULL)
+   {
+      MESSAGE_DEBUG("Read unmapped region: $%4X\n", address);
+      return 0xFF;
+   }
+
+   /* Paged memory */
+   return page[address & 0xFFF];
 }
 
-/* write a byte of data to 6502 memory */
+/* write a byte of data to 6502 memory space */
 INLINE void mem_writebyte(uint32 address, uint8 value)
 {
-   nes6502_memwrite *mw;
+   uint8 *page = cpu.mem_pages[address >> NES6502_BANKSHIFT];
 
    /* RAM */
    if (address < 0x2000)
    {
-      ram[address & 0x7FF] = value;
+      page[address & 0x7FF] = value;
       return;
    }
-   /* check memory range handlers */
-   else
+
+   /* Special memory handlers */
+   for (nes6502_memwrite *mw = cpu.write_handlers; mw->write_func != NULL; mw++)
    {
-      for (mw = cpu.write_handlers; mw->min_range != NULL; mw++)
+      if (address >= mw->min_range && address <= mw->max_range)
       {
-         if (address >= mw->min_range && address <= mw->max_range)
-         {
-            mw->write_func(address, value);
-            return;
-         }
+         mw->write_func(address, value);
+         return;
       }
    }
 
-   /* write to paged memory */
-   cpu.mem_page[address >> NES6502_BANKSHIFT][address & NES6502_BANKMASK] = value;
+   /* Unmapped region */
+   if (page == NULL)
+   {
+      MESSAGE_DEBUG("Write to unmapped region: $%2X to $%4X\n", address, value);
+      return;
+   }
+
+   /* Paged memory */
+   page[address & 0xFFF] = value;
+}
+
+/* Get byte from cpu address space */
+IRAM_ATTR uint8 nes6502_getbyte(uint16 address)
+{
+   return mem_readbyte(address);
+}
+
+/* Write byte to cpu address space */
+IRAM_ATTR void nes6502_putbyte(uint16 address, uint8 value)
+{
+   mem_writebyte(address, value);
+}
+
+/* Set 4KB memory page */
+IRAM_ATTR void nes6502_setpage(uint16 page, uint8 *ptr)
+{
+   cpu.mem_pages[page] = ptr;
+   cpu.ram = cpu.mem_pages[0];
+}
+
+/* Get 4KB memory page */
+IRAM_ATTR uint8 *nes6502_getpage(uint16 page)
+{
+   return cpu.mem_pages[page];
 }
 
 /* set the current context */
 void nes6502_setcontext(nes6502_t *context)
 {
-   int loop;
-
    ASSERT(context);
 
    cpu = *context;
-
-   /* set dead page for all pages not pointed at anything */
-   for (loop = 0; loop < NES6502_NUMBANKS; loop++)
-   {
-      if (NULL == cpu.mem_page[loop])
-         cpu.mem_page[loop] = null_page;
-   }
-
-   ram = cpu.mem_page[0];  /* quick zero-page/RAM references */
-   stack = ram + STACK_OFFSET;
 }
 
 /* get the current context */
 void nes6502_getcontext(nes6502_t *context)
 {
-   int loop;
-
    ASSERT(context);
 
    *context = cpu;
-
-   /* reset dead pages to null */
-   for (loop = 0; loop < NES6502_NUMBANKS; loop++)
-   {
-      if (null_page == context->mem_page[loop])
-         context->mem_page[loop] = NULL;
-   }
-}
-
-/* DMA a byte of data from ROM */
-IRAM_ATTR uint8 nes6502_getbyte(uint32 address)
-{
-   // return mem_readbyte(address);
-   return bank_readbyte(address);
-}
-
-IRAM_ATTR void nes6502_putbyte(uint32 address, uint8 value)
-{
-   // mem_writebyte(address, value);
-   bank_writebyte(address, value);
 }
 
 /* get number of elapsed cycles */
-IRAM_ATTR uint32 nes6502_getcycles(bool reset_flag)
+IRAM_ATTR uint32 nes6502_getcycles()
 {
-   uint32 cycles = cpu.total_cycles;
-
-   if (reset_flag)
-      cpu.total_cycles = 0;
-
-   return cycles;
+   return cpu.total_cycles;
 }
 
 #define MIN(a,b)    (((a) < (b)) ? (a) : (b))
@@ -1221,14 +1202,14 @@ IRAM_ATTR uint32 nes6502_getcycles(bool reset_flag)
 #define OPCODE_NEXT \
    if (remaining_cycles <= 0) break; \
    DISASSEMBLE; \
-   goto *opcode_table[bank_readbyte(PC++)];
+   goto *opcode_table[fast_readbyte(PC++)];
 
 #else /* !NES6502_JUMPTABLE */
 
 #define OPCODE(xx, x...)  case 0x##xx: x; break;
 #define OPCODE_NEXT \
    DISASSEMBLE;  \
-   switch (bank_readbyte(PC++))
+   switch (fast_readbyte(PC++))
 
 #endif /* !NES6502_JUMPTABLE */
 
@@ -1601,7 +1582,7 @@ void nes6502_reset(void)
    cpu.p_reg = Z_FLAG | R_FLAG | I_FLAG;     /* Reserved bit always 1 */
    cpu.int_pending = 0;                      /* No pending interrupts */
    cpu.int_latency = 0;                      /* No latent interrupts */
-   cpu.pc_reg = bank_readword(RESET_VECTOR); /* Fetch reset vector */
+   cpu.pc_reg = fast_readword(RESET_VECTOR); /* Fetch reset vector */
    cpu.burn_cycles = RESET_CYCLES;
    cpu.jammed = false;
 }
@@ -1642,13 +1623,41 @@ IRAM_ATTR void nes6502_irq(void)
 }
 
 /* Set dead cycle period */
-void nes6502_burn(int cycles)
+IRAM_ATTR void nes6502_burn(int cycles)
 {
    cpu.burn_cycles += cycles;
 }
 
 /* Release our timeslice */
-void nes6502_release(void)
+IRAM_ATTR void nes6502_release(void)
 {
    remaining_cycles = 0;
+}
+
+/* Create a nes6502 object */
+nes6502_t *nes6502_create(void *ram)
+{
+   nes6502_t *temp;
+
+   temp = &cpu;
+   // temp = malloc(sizeof(nes6502_t));
+   // if (NULL == temp)
+   //    return NULL;
+
+   memset(temp, 0, sizeof(nes6502_t));
+
+   nes6502_setpage(0, ram);
+   nes6502_setpage(1, ram);
+
+   return temp;
+}
+
+/* Destroy a nes6502 object */
+void nes6502_destroy(nes6502_t **src_cpu)
+{
+   if (*src_cpu)
+   {
+      free(*src_cpu);
+      *src_cpu = NULL;
+   }
 }

nesemu-go/components/nofrendo/cpu/nes6502.h
@@ -75,7 +75,7 @@ typedef struct
 
 typedef struct
 {
-   uint8 *mem_page[NES6502_NUMBANKS];  /* memory page pointers */
+   uint8 *mem_pages[NES6502_NUMBANKS];  /* memory page pointers */
 
    nes6502_memread *read_handlers;
    nes6502_memwrite *write_handlers;
@@ -84,6 +84,8 @@ typedef struct
    uint8 a_reg, p_reg;
    uint8 x_reg, y_reg;
    uint8 s_reg;
+
+   uint8 *ram; /* shortcut to page 0 (zero page + stack) */
 
    uint8 jammed;  /* is processor jammed? */
 
@@ -101,11 +103,17 @@ extern void nes6502_reset(void);
 extern int nes6502_execute(int total_cycles);
 extern void nes6502_nmi(void);
 extern void nes6502_irq(void);
-extern uint8 nes6502_getbyte(uint32 address);
-extern void nes6502_putbyte(uint32 address, uint8 value);
-extern uint32 nes6502_getcycles(bool reset_flag);
+extern uint32 nes6502_getcycles();
 extern void nes6502_burn(int cycles);
 extern void nes6502_release(void);
+
+extern uint8 nes6502_getbyte(uint16 address);
+extern void nes6502_putbyte(uint16 address, uint8 value);
+extern uint8 *nes6502_getpage(uint16 page);
+extern void nes6502_setpage(uint16 page, uint8 *ptr);
+
+extern nes6502_t *nes6502_create(void *ram);
+extern void nes6502_destroy(nes6502_t **src_cpu);
 
 /* Context get/set */
 extern void nes6502_setcontext(nes6502_t *cpu);

commit 2e3b4bb18d0e3628741eb9bc1400855cb53833ca

NES: Fixed 32K mapping (Pirates!) broken in cc97d3c + faster mem access: 2K pages and negative offsets = less bitwise ops

nesemu-go/components/nofrendo/cpu/nes6502.c
@@ -32,7 +32,7 @@
 /* internal CPU context */
 static nes6502_t cpu;
 static int remaining_cycles = 0;
-static uint8 *zp, *stack;
+static mem_t *mem;
 
 // #define NES6502_TESTOPS
 #define NES6502_JUMPTABLE
@@ -65,7 +65,7 @@ static uint8 *zp, *stack;
 /* Immediate */
 #define IMMEDIATE_BYTE(value) \
 { \
-   value = fast_readbyte(PC++); \
+   value = readbyte(PC++); \
 }
 
 /* Absolute */
@@ -235,8 +235,8 @@ static uint8 *zp, *stack;
 
 
 /* Stack push/pull */
-#define PUSH(value)             stack[S--] = (uint8) (value)
-#define PULL()                  stack[++S]
+#define PUSH(value)             cpu.stack[S--] = (uint8) (value)
+#define PULL()                  cpu.stack[++S]
 
 
 /*
@@ -633,7 +633,7 @@ static uint8 *zp, *stack;
    temp = readword(PC); \
    /* bug in crossing page boundaries */ \
    if (0xFF == (temp & 0xFF)) \
-      PC = (fast_readbyte(temp & 0xFF00) << 8) | fast_readbyte(temp); \
+      PC = (readbyte(temp & 0xFF00) << 8) | readbyte(temp); \
    else \
       JUMP(temp); \
    ADD_CYCLES(5); \
@@ -1057,15 +1057,9 @@ static uint8 *zp, *stack;
 ** Zero-page helper macros
 */
 
-#define ZP_READBYTE(addr)          (zp[(addr)])
-#define ZP_WRITEBYTE(addr, value)  (zp[(addr)] = (uint8) (value))
-#define ZP_READWORD(addr)          PAGE_READWORD(zp, addr)
-
-#ifdef HOST_LITTLE_ENDIAN
-#define PAGE_READWORD(page, addr)  (*(uint16 *)((page) + (addr)))
-#else
-#define PAGE_READWORD(page, addr)  (page[(addr) + 1] << 8 | page[(addr)])
-#endif
+#define ZP_READBYTE(addr)          (cpu.zp[(addr)])
+#define ZP_WRITEBYTE(addr, value)  (cpu.zp[(addr)] = (uint8) (value))
+#define ZP_READWORD(addr)          PAGE_READWORD(cpu.zp, addr)
 
 /*
 ** Memory-related macros
@@ -1083,53 +1077,33 @@ static uint8 *zp, *stack;
 
 #ifdef NES6502_FASTMEM
 
-INLINE uint8 fast_readbyte(uint32 address)
-{
-   uint8 *page = cpu.mem->pages[address >> MEM_PAGESHIFT];
-   return page ? page[address & MEM_PAGEMASK] : 0xFF;
-}
-
 INLINE uint8 readbyte(uint32 address)
 {
-   uint8 *page = cpu.mem->pages_read[address >> MEM_PAGESHIFT];
-
-   if (MEM_PAGE_IS_VALID_PTR(page))
-   {
-      if (address < 0x2000) return page[address & MEM_RAMMASK];
-      else                  return page[address & MEM_PAGEMASK];
+   if (address < 0x2000 || address >= 0x8000) {
+      return mem->pages[address >> MEM_PAGESHIFT][address];
    }
    return mem_getbyte(address);
 }
 
-INLINE void writebyte(uint32 address, uint8 value)
-{
-   uint8 *page = cpu.mem->pages_write[address >> MEM_PAGESHIFT];
-
-   if (MEM_PAGE_IS_VALID_PTR(page))
-   {
-      if (address < 0x2000) page[address & MEM_RAMMASK] = value;
-      else                  page[address & MEM_PAGEMASK] = value;
-   }
-   else
-      mem_putbyte(address, value);
-}
-
 INLINE uint32 readword(uint32 address)
 {
-   uint8 *page = cpu.mem->pages_read[address >> MEM_PAGESHIFT];
-
-   if (MEM_PAGE_IS_VALID_PTR(page))
-   {
-      if (address < 0x2000) return PAGE_READWORD(page, address & MEM_RAMMASK);
-      else                  return PAGE_READWORD(page, address & MEM_PAGEMASK);
+   if (address < 0x2000 || address >= 0x8000) {
+      return PAGE_READWORD(mem->pages[address >> MEM_PAGESHIFT], address);
    }
+   return mem_getword(address);
+}
 
-   return mem_getword(address + 1);
+INLINE void writebyte(uint32 address, uint8 value)
+{
+   if (address < 0x2000) {
+      mem->ram[address & 0x7FF] = value;
+   } else {
+      mem_putbyte(address, value);
+   }
 }
 
 #else /* !NES6502_FASTMEM */
 
-#define fast_readbyte(a) mem_getbyte(a)
 #define writebyte(a, v) mem_putbyte(a, v)
 #define readbyte(a) mem_getbyte(a)
 #define readword(a) mem_getword(a)
@@ -1150,14 +1124,14 @@ INLINE uint32 readword(uint32 address)
 #define OPCODE_NEXT \
    if (remaining_cycles <= 0) break; \
    DISASSEMBLE; \
-   goto *opcode_table[fast_readbyte(PC++)];
+   goto *opcode_table[readbyte(PC++)];
 
 #else /* !NES6502_JUMPTABLE */
 
 #define OPCODE(xx, x...)  case 0x##xx: x; break;
 #define OPCODE_NEXT \
    DISASSEMBLE;  \
-   switch (fast_readbyte(PC++))
+   switch (readbyte(PC++))
 
 #endif /* !NES6502_JUMPTABLE */
 
@@ -1169,9 +1143,6 @@ void nes6502_setcontext(nes6502_t *context)
 {
    ASSERT(context);
    cpu = *context;
-
-   zp    = cpu.mem->pages[0];
-   stack = cpu.mem->pages[0] + STACK_OFFSET;
 }
 
 /* get the current context */
@@ -1199,9 +1170,6 @@ IRAM_ATTR int nes6502_execute(int timeslice_cycles)
    uint32 temp, addr; /* for macros */
    uint8 btemp, baddr; /* for macros */
    uint8 data;
-
-   // zp    = cpu.mem->pages[0];
-   // stack = cpu.mem->pages[0] + STACK_OFFSET;
 
    DECLARE_LOCAL_REGS();
    GET_GLOBAL_REGS();
@@ -1555,10 +1523,10 @@ IRAM_ATTR int nes6502_execute(int timeslice_cycles)
 /* Issue a CPU Reset */
 void nes6502_reset(void)
 {
-   cpu.p_reg = Z_FLAG | R_FLAG | I_FLAG;     /* Reserved bit always 1 */
-   cpu.int_pending = 0;                      /* No pending interrupts */
-   cpu.int_latency = 0;                      /* No latent interrupts */
-   cpu.pc_reg = readword(RESET_VECTOR);  /* Fetch reset vector */
+   cpu.p_reg = Z_FLAG | R_FLAG | I_FLAG;  /* Reserved bit always 1 */
+   cpu.int_pending = 0;                   /* No pending interrupts */
+   cpu.int_latency = 0;                   /* No latent interrupts */
+   cpu.pc_reg = readword(RESET_VECTOR);   /* Fetch reset vector */
    cpu.burn_cycles = RESET_CYCLES;
    cpu.jammed = false;
 }
@@ -1611,7 +1579,7 @@ IRAM_ATTR void nes6502_release(void)
 }
 
 /* Create a nes6502 object */
-nes6502_t *nes6502_create(mem_t *mem)
+nes6502_t *nes6502_create(mem_t *_mem)
 {
    nes6502_t *temp;
 
@@ -1621,8 +1589,11 @@ nes6502_t *nes6502_create(mem_t *mem)
    //    return NULL;
 
    memset(temp, 0, sizeof(nes6502_t));
-   temp->mem = mem;
 
+   cpu.zp    = _mem->ram + 0x000; // Page 0
+   cpu.stack = _mem->ram + 0x100; // Page 1
+
+   mem = _mem; // For FASTMEM
 
    return temp;
 }

nesemu-go/components/nofrendo/cpu/nes6502.h
@@ -53,9 +53,6 @@
 #define  NMI_MASK       0x01
 #define  IRQ_MASK       0x02
 
-/* Stack is located on 6502 page 1 */
-#define  STACK_OFFSET   0x0100
-
 typedef struct
 {
    uint32 pc_reg;
@@ -63,7 +60,7 @@ typedef struct
    uint8 x_reg, y_reg;
    uint8 s_reg;
 
-   mem_t *mem;
+   uint8 *zp, *stack;
 
    uint8 jammed;  /* is processor jammed? */
 

nesemu-go/components/nofrendo/nes/nes_mem.c
@@ -119,26 +119,33 @@ void mem_refresh()
    ASSERT(num_write_handlers <= MEM_HANDLERS_MAX);
 }
 
-/* Set 4KB memory page */
+/* Set 2KB memory page */
 IRAM_ATTR void mem_setpage(uint16 page, uint8 *ptr)
 {
-   mem.pages[page] = ptr;
+   mem.pages[page] = ptr - (page * MEM_PAGESIZE);
 
    if (!MEM_PAGE_HAS_HANDLERS(mem.pages_read[page]))
    {
-      mem.pages_read[page] = ptr;
+      mem.pages_read[page] = mem.pages[page];
    }
 
    if (!MEM_PAGE_HAS_HANDLERS(mem.pages_write[page]))
    {
-      mem.pages_write[page] = ptr;
+      mem.pages_write[page] = mem.pages[page];
    }
 }
 
-/* Get 4KB memory page */
+/* Get 2KB memory page */
 uint8 *mem_getpage(uint16 page)
 {
-   return mem.pages[page];
+   uint8 *page_ptr = mem.pages[page];
+
+   if (MEM_PAGE_IS_VALID_PTR(page_ptr))
+   {
+      return page_ptr + (page * MEM_PAGESIZE);
+   }
+
+   return page_ptr;
 }
 
 /* read a byte of 6502 memory space */
@@ -163,15 +170,10 @@ IRAM_ATTR uint8 mem_getbyte(uint32 address)
       MESSAGE_DEBUG("Read unmapped region: $%4X\n", address);
       return 0xFF;
    }
-   /* RAM */
-   else if (address < 0x2000)
-   {
-      return page[address & MEM_RAMMASK];
-   }
    /* Paged memory */
    else
    {
-      return page[address & MEM_PAGEMASK];
+      return page[address];
    }
 }
 
@@ -199,15 +201,10 @@ IRAM_ATTR void mem_putbyte(uint32 address, uint8 value)
    {
       MESSAGE_DEBUG("Write to unmapped region: $%2X to $%4X\n", address, value);
    }
-   /* RAM */
-   else if (address < 0x2000)
-   {
-      page[address & MEM_RAMMASK] = value;
-   }
    /* Paged memory */
    else
    {
-      page[address & MEM_PAGEMASK] = value;
+      page[address] = value;
    }
 }
 
@@ -221,7 +218,9 @@ void mem_reset()
    memset(&mem.pages, 0, sizeof(mem.pages));
    memset(&mem.ram, 0, sizeof(mem.ram));
    mem_setpage(0, mem.ram);
-   mem_setpage(1, mem.ram);
+   mem_setpage(1, mem.ram); // $800 - $FFF mirror
+   mem_setpage(2, mem.ram); // $1000 - $07FF mirror
+   mem_setpage(3, mem.ram); // $1800 - $1FFF mirror
    mem_refresh();
 }
 

nesemu-go/components/nofrendo/nes/nes_mem.h
@@ -28,14 +28,12 @@
 
 #include <nofrendo.h>
 
-#define MEM_PAGEBITS  4
+#define MEM_PAGEBITS  5
 #define MEM_PAGESHIFT (16 - MEM_PAGEBITS)
 #define MEM_PAGECOUNT (1 << MEM_PAGEBITS)
 #define MEM_PAGESIZE  (0x10000 / MEM_PAGECOUNT)
-#define MEM_PAGEMASK  (MEM_PAGESIZE - 1)
 
 #define MEM_RAMSIZE   0x800
-#define MEM_RAMMASK   0x7FF
 
 // This is kind of a hack, but for speed...
 #define MEM_PAGE_USE_HANDLERS ((uint8*)1)

nesemu-go/components/nofrendo/nes/nes_mmc.c
@@ -86,37 +86,28 @@ void mmc_bankptr(int size, uint32 address, int bank, uint8 *ptr)
       if (bank == MMC_LASTBANK)
          bank = MMC_LAST8KPRG;
       base = ptr + ((bank % MMC_8KPRG) << 13);
-      mem_setpage(page + 0, base);
-      mem_setpage(page + 1, base + 0x1000);
       break;
 
    case 16:
       if (bank == MMC_LASTBANK)
          bank = MMC_LAST16KPRG;
       base = ptr + ((bank % MMC_16KPRG) << 14);
-      mem_setpage(page + 0, base);
-      mem_setpage(page + 1, base + 0x1000);
-      mem_setpage(page + 2, base + 0x2000);
-      mem_setpage(page + 3, base + 0x3000);
       break;
 
    case 32:
       if (bank == MMC_LASTBANK)
          bank = MMC_LAST32KPRG;
       base = ptr + ((bank % MMC_32KPRG) << 15);
-      mem_setpage(page + 0, base);
-      mem_setpage(page + 1, base + 0x1000);
-      mem_setpage(page + 2, base + 0x2000);
-      mem_setpage(page + 3, base + 0x3000);
-      mem_setpage(page + 1, base + 0x4000);
-      mem_setpage(page + 2, base + 0x5000);
-      mem_setpage(page + 3, base + 0x6000);
-      mem_setpage(page + 3, base + 0x7000);
       break;
 
    default:
       MESSAGE_ERROR("MMC: Invalid bank size! Addr: $%04X Bank: %d Size: %d\n", address, bank, size);
       abort();
+   }
+
+   for (int i = 0; i < (size * 0x400 / MEM_PAGESIZE); i++)
+   {
+      mem_setpage(page + i, base + i * MEM_PAGESIZE);
    }
 }
 
@@ -187,9 +178,11 @@ static void mmc_setpages(void)
    /* Switch Save RAM into CPU space */
    mmc_bankwram(8, 0x6000, 0);
 
-   /* Switch PRG and CHR into CPU space */
+   /* Switch PRG ROM into CPU space */
    mmc_bankrom(16, 0x8000, 0);
    mmc_bankrom(16, 0xC000, MMC_LASTBANK);
+
+   /* Switch CHR ROM/RAM into CPU space */
    mmc_bankvrom(8, 0x0000, 0);
 
    if (mmc.cart->flags & ROM_FLAG_FOURSCREEN)

nesemu-go/components/nofrendo/nes/nes_ppu.c
@@ -342,7 +342,7 @@ IRAM_ATTR void ppu_write(uint32 address, uint8 value)
       ppu.bg_base = (value & PPU_CTRL0F_BGADDR) ? 0x1000 : 0;
       ppu.obj_base = (value & PPU_CTRL0F_OBJADDR) ? 0x1000 : 0;
       ppu.vaddr_inc = (value & PPU_CTRL0F_ADDRINC) ? 32 : 1;
-      ppu.tile_nametab = value & PPU_CTRL0F_NAMETAB;
+      ppu.base_nametab = value & PPU_CTRL0F_NAMETAB;
 
       /* Mask out bits 10 & 11 in the ppu latch */
       ppu.vaddr_latch &= ~0x0C00;
@@ -613,7 +613,7 @@ INLINE void draw_oamtile(uint8 *surface, uint8 attrib, uint32 pattern, const uin
 INLINE void ppu_renderbg(uint8 *vidbuf)
 {
    uint8 *bmp_ptr;
-   uint32 refresh_vaddr, tile_addr, bg_offset, attrib_base, attrib_addr;
+   int32 refresh_vaddr, tile_addr, bg_offset, attrib_base, attrib_addr;
    uint8 tile_index, tile_num, x_tile, y_tile, col_high, attrib, attrib_shift;
 
    /* draw a line of transparent background color if bg is disabled */
@@ -680,9 +680,11 @@ INLINE void ppu_renderbg(uint8 *vidbuf)
    /* Blank left hand column if need be */
    if (ppu.bg_mask)
    {
-      uint32 bg_clear = FULLBG | FULLBG << 8 | FULLBG << 16 | FULLBG << 24;
-      ((uint32 *) vidbuf)[0] = bg_clear;
-      ((uint32 *) vidbuf)[1] = bg_clear;
+      for (int i = 0; i < 8; i++)
+      {
+         vidbuf[i] = FULLBG;
+      }
+      // memset(vidbuf, 8, FULLBG);
    }
 }
 
@@ -780,10 +782,10 @@ IRAM_ATTR bool ppu_enabled(void)
    return (ppu.bg_on || ppu.obj_on);
 }
 
-IRAM_ATTR void ppu_endscanline(int scanline)
+IRAM_ATTR void ppu_endscanline(void)
 {
    /* modify vram address at end of scanline */
-   if (scanline < 240 && (ppu.bg_on || ppu.obj_on))
+   if (ppu.scanline < 240 && (ppu.bg_on || ppu.obj_on))
    {
       int ytile;
 
@@ -822,6 +824,8 @@ IRAM_ATTR void ppu_checknmi(void)
 
 IRAM_ATTR void ppu_scanline(bitmap_t *bmp, int scanline, bool draw_flag)
 {
+   ppu.scanline = scanline;
+
    if (scanline < 240)
    {
       /* Lower the Max Sprite per scanline flag */

nesemu-go/components/nofrendo/nes/nes_ppu.h
@@ -100,21 +100,19 @@ typedef struct ppu_s
    rgb_t curpal[256];
 
    /* hardware registers */
-   uint8 ctrl0, ctrl1, stat, oam_addr;
-   uint32 vaddr, vaddr_latch;
-   int tile_xofs, flipflop;
-   int vaddr_inc;
-   uint32 tile_nametab;
+   uint8 ctrl0, ctrl1, stat, oam_addr, base_nametab;
+   uint8 latch, vdata_latch, tile_xofs, flipflop;
+   int32 vaddr, vaddr_latch, vaddr_inc;
 
-   uint32 obj_height, obj_base, bg_base;
-
-   bool bg_on, obj_on;
+   int32 obj_height, obj_base, bg_base;
    bool obj_mask, bg_mask;
-
-   uint8 latch, vdata_latch;
+   bool bg_on, obj_on;
 
    bool strikeflag;
    uint32 strike_cycle;
+
+   // Last or currently rendering scanline
+   int scanline;
 
    /* callbacks for naughty mappers */
    ppu_latchfunc_t latchfunc;
@@ -142,7 +140,7 @@ extern uint8 *ppu_getpage(int page);
 extern void ppu_reset();
 extern bool ppu_enabled(void);
 extern void ppu_scanline(bitmap_t *bmp, int scanline, bool draw_flag);
-extern void ppu_endscanline(int scanline);
+extern void ppu_endscanline();
 extern void ppu_checknmi();
 
 extern ppu_t *ppu_create(void);

nesemu-go/components/nofrendo/nes/nes_state.c
@@ -206,7 +206,7 @@ int save_mapperblock(nes_t *state, SNSS_FILE *snssFile)
 
    /* TODO: snss spec should be updated, using 4kB ROM pages.. */
    for (i = 0; i < 4; i++)
-      snssFile->mapperBlock.prgPages[i] = (mem_getpage((i + 4) * 2) - state->rominfo->rom) >> 13;
+      snssFile->mapperBlock.prgPages[i] = (mem_getpage((i + 4) * 4) - state->rominfo->rom) >> 13;
 
    if (state->rominfo->vrom_banks)
    {

nesemu-go/components/nofrendo/nofrendo.h
@@ -50,17 +50,14 @@
 
 #undef  PATH_MAX
 #define PATH_MAX  512
+#undef  PATH_SEP
+#define PATH_SEP  '/'
 
 #ifndef __PACKED__
 #define __PACKED__  __attribute__ ((packed))
 #endif
 
-#ifndef PATH_SEP
-#define PATH_SEP    '/'
-#endif
-
 #define INLINE              static inline __attribute__((__always_inline__))
-#define ZERO_LENGTH 0
 
 #ifdef NOFRENDO_DEBUG
 #define UNUSED(x)