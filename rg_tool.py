89d797d5b68598f26a47968f57f9296f3382c192
Alex Duchesne <alex@netvps.ca>
2020-08-27 16:40:45 -0400

    Formatted rg_tool.py with black

--- rg_tool.py
+++ rg_tool.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from os import path, getenv, getcwd, chdir
-import sys, math, zlib, struct
-import subprocess, argparse, shutil
+import sys, math, subprocess, argparse, shutil
 
 IDF_PATH = getenv("IDF_PATH")
 PRJ_PATH = sys.path[0]  # getcwd()
@@ -18,60 +17,21 @@ chdir(PRJ_PATH)
 
 exec(read_file("rg_config.py"))
 
-parser = argparse.ArgumentParser(description="Retro-Go build tool")
-parser.add_argument("command", choices=["build", "clean", "release", "mkfw", "flash", "flashmon", "monitor"])
-parser.add_argument("targets", nargs="*", default="all", choices=["all"] + list(PROJECT_APPS.keys()))
-parser.add_argument("--use-make", action="store_const", const=True, help="Use legacy make build system")
-parser.add_argument("--port", default="COM3", help="Serial port to use for flash and monitor")
-parser.add_argument("--offset", default="0x100000", help="Flash offset where %s is installed" % PROJECT_NAME)
-parser.add_argument("--app-offset", default=None, help="Absolute offset where target will be flashed")
-args = parser.parse_args()
 
-command = args.command
-targets = args.targets if not "all" in args.targets else PROJECT_APPS.keys()
-prj_offset = int(args.offset, 0)
-app_offset = int(args.app_offset, 0) if args.app_offset else None
-
-def build_firmware():
-    # Header:
-    #   "ODROIDGO_FIRMWARE_V00_01"  24 bytes
-    #   Firmware Description        40 bytes
-    #   RAW565 86x48 tile           8256 bytes
-    # Partition [, ...]:
-    #   Type                        1 byte
-    #   Subtype                     1 byte
-    #   Padding                     2 bytes
-    #   Label                       16 bytes
-    #   Flags                       4 bytes
-    #   Size                        4 bytes
-    #   Data length                 4 bytes
-    # Footer:
-    #   CRC32                       4 bytes
-
+def build_firmware(targets):
     chdir(PRJ_PATH)
+    args = [
+        sys.executable,
+        "tools/mkfw.py",
+        ("%s_%s.fw" % (PROJECT_NAME, PROJECT_VER)).lower(),
+        ("%s %s" % (PROJECT_NAME, PROJECT_VER)),
+        PROJECT_TILE
+    ]
+    for target in targets:
+        part = PROJECT_APPS[target]
+        args += [str(0), str(part[0]), str(part[1]), target, path.join(target, "build", target + ".bin")]
 
-    description = ("%s %s" % (PROJECT_NAME, PROJECT_VER)).encode()
-    filename = ("%s_%s.fw" % (PROJECT_NAME, PROJECT_VER)).lower()
-
-    fw_data = struct.pack("<24s 40s 8256s", b"ODROIDGO_FIRMWARE_V00_01", description, read_file(PROJECT_TILE))
-
-    for key in targets:
-        data = read_file(path.join(key, "build", key + ".bin"))
-        part = PROJECT_APPS[key]
-        size = max(part[1], math.ceil(len(data) / 0x10000) * 0x10000)
-        if size > part[1]:
-            print("WARNING: Partition %s will be expanded by %d bytes" % (key, size - part[1]))
-        fw_data += struct.pack("<BBxx16sIII", 0, part[0], key.encode(), 0, size, len(data))
-        fw_data += data
-        print("Partition: %s %d" % (key, size))
-
-    fw_data += struct.pack("I", zlib.crc32(fw_data))
-
-    fp = open(filename, "wb")
-    fp.write(fw_data)
-    fp.close()
-
-    print("%s successfully created.\n" % filename)
+    subprocess.run(args, check=True)
 
 
 def clean_app(target):
@@ -83,31 +43,33 @@ def clean_app(target):
         print("Nothing to do.\n")
 
 
-def build_app(target):
+def build_app(target, use_make):
     print("Building app '%s'" % target)
     chdir(path.join(PRJ_PATH, target))
-    if args.use_make:
+    if use_make:
         subprocess.run("make -j 6 app", shell=True, check=True)
     else:
         subprocess.run("idf.py app", shell=True, check=True)
     print("Done.\n")
 
 
-def flash_app(target):
+def find_app(target, offset, app_offset=None):
     if app_offset != None:
-        offset = app_offset
-    else:
-        offset = prj_offset
-        for key, value in PROJECT_APPS.items():
-            if key == target: break
-            offset += (math.ceil(value[1] / 0x10000) * 0x10000)
+        return int(str(app_offset), 0)
+    offset = int(str(offset), 0)
+    for key, value in PROJECT_APPS.items():
+        if key == target:
+            return offset
+        offset += math.ceil(value[1] / 0x10000) * 0x10000
 
+
+def flash_app(target, port, offset=0x100000):
     print("Flashing app '%s' at offset %s" % (target, hex(offset)))
     subprocess.run([
         sys.executable,
         path.join(IDF_PATH, "components", "esptool_py", "esptool", "esptool.py"),
         "--chip", "esp32",
-        "--port", args.port,
+        "--port", port,
         "--baud", "1152000",
         "--before", "default_reset",
         "write_flash",
@@ -115,38 +77,63 @@ def flash_app(target):
         "--flash_freq", "80m",
         "--flash_size", "detect",
         hex(offset),
-        path.join(PRJ_PATH, target, "build", target + ".bin")
+        path.join(PRJ_PATH, target, "build", target + ".bin"),
     ], check=True)
     print("Done.\n")
 
 
-def monitor_app(target):
+def monitor_app(target, port):
     print("Starting monitor for app '%s'" % target)
     subprocess.run([
         sys.executable,
         path.join(IDF_PATH, "tools", "idf_monitor.py"),
-        "--port", args.port,
-        path.join(PRJ_PATH, target, "build", target + ".elf")
+        "--port", port,
+        path.join(PRJ_PATH, target, "build", target + ".elf"),
     ])
 
 
+parser = argparse.ArgumentParser(description="Retro-Go build tool")
+parser.add_argument(
+    "command", choices=["build", "clean", "release", "mkfw", "flash", "flashmon", "monitor"],
+)
+parser.add_argument(
+    "targets", nargs="*", default="all", choices=["all"] + list(PROJECT_APPS.keys())
+)
+parser.add_argument(
+    "--use-make", action="store_const", const=True, help="Use legacy make build system"
+)
+parser.add_argument(
+    "--port", default="COM3", help="Serial port to use for flash and monitor"
+)
+parser.add_argument(
+    "--offset", default="0x100000", help="Flash offset where %s is installed" % PROJECT_NAME,
+)
+parser.add_argument(
+    "--app-offset", default=None, help="Absolute offset where target will be flashed"
+)
+args = parser.parse_args()
+
+command = args.command
+targets = args.targets if not "all" in args.targets else PROJECT_APPS.keys()
+
 if command == "clean" or command == "release":
-    for key in targets:
-        clean_app(key)
+    for target in targets:
+        clean_app(target)
 
 if command == "build" or command == "release":
-    for key in targets:
-        build_app(key)
+    for target in targets:
+        build_app(target, args.use_make)
 
 if command == "release" or command == "mkfw":
-    build_firmware()
+    build_firmware(targets)
 
 if command == "flash" or command == "flashmon":
-    for key in targets:
-        flash_app(key)
+    for target in targets:
+        offset = find_app(target, args.offset, args.app_offset)
+        flash_app(target, args.port, offset)
 
 if command == "monitor" or command == "flashmon":
-    monitor_app(targets[0])
+    monitor_app(targets[0], args.port)
 
 
 print("All done!")
---
+++ tools/mkfw.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import sys, math, zlib, struct
+
+def readfile(filepath):
+    with open(filepath, "rb") as f: return f.read()
+
+if len(sys.argv) < 4:
+    exit("usage: mkfw.py output_file description tile <type subtype length label binary> [, ...]")
+
+fw_name = sys.argv[1]
+
+fw_data = struct.pack(
+    "<24s40s8256s",
+    b"ODROIDGO_FIRMWARE_V00_01",
+    sys.argv[2].encode(),
+    readfile(sys.argv[3])
+)
+
+count = 0
+pos = 4
+
+while pos < len(sys.argv):
+    partype = int(sys.argv[pos+0], 0)
+    subtype = int(sys.argv[pos+1], 0)
+    length = int(sys.argv[pos+2], 0)
+    label = sys.argv[pos+3]
+    data = readfile(sys.argv[pos+4])
+    pos += 5
+
+    print("[%d]: type=%d, subtype=%d, length=%d, label=%s" % (count, partype, subtype, length, label));
+
+    size = max(length, math.ceil(len(data) / 0x10000) * 0x10000)
+    if size > length:
+        print(" > WARNING: Partition will be expanded by %d bytes" % (size - length))
+
+    fw_data += struct.pack("<BBxx16sIII", partype, subtype, label.encode(), 0, size, len(data))
+    fw_data += data
+    count += 1
+
+fw_data += struct.pack("I", zlib.crc32(fw_data))
+
+fp = open(fw_name, "wb")
+fp.write(fw_data)
+fp.close()
+
+print("%s successfully created.\n" % fw_name)
+
