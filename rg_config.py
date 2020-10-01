8782a89b24e6f418a9a25b4055be4e91affb1629
Alex Duchesne <alex@netvps.ca>
2020-10-01 14:50:00 -0400

    Reverted partition sizes to match 1.18 .fw size

--- rg_config.py
+++ rg_config.py
@@ -12,9 +12,9 @@ PROJECT_APPS = {
   # Note: Size will be adjusted if needed but flashmon needs accurate values to work correctly
   # Project name   Sub, Size
   'retro-go':     [16,  524288],
-  'nofrendo-go':  [17,  524288],
-  'gnuboy-go':    [18,  524288],
+  'nofrendo-go':  [17,  458752],
+  'gnuboy-go':    [18,  458752],
   'smsplusgx-go': [19,  524288],
-  'huexpress-go': [20,  524288],
-  'handy-go':     [21,  524288],
+  'huexpress-go': [20,  458752],
+  'handy-go':     [21,  458752],
 }
