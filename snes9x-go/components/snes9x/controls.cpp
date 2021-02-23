ducalex/retro-go

commit a45b2fbedf4dea3e060dcc7a47a5ee3ce58dd41f

SNES: Simplified controls.cpp further to save flash (10K) and ram (1.5K)

snes9x-go/components/snes9x/controls.cpp
@@ -4,51 +4,19 @@
    For further information, consult the LICENSE file in the root directory.
 \*****************************************************************************/
 
-#include <map>
-#include <algorithm>
-#include <string>
-#include <assert.h>
-#include <ctype.h>
-
 #include "snes9x.h"
 #include "memory.h"
 #include "apu/apu.h"
 #include "snapshot.h"
 #include "controls.h"
 
-using namespace	std;
-
-#define NONE					(-2)
-#define JOYPAD0					0
-#define JOYPAD1					1
-#define JOYPAD2					2
-#define JOYPAD3					3
-#define JOYPAD4					4
-#define JOYPAD5					5
-#define JOYPAD6					6
-#define JOYPAD7					7
-#define NUMCTLS					8 // This must be LAST
-
-#define MAP_UNKNOWN				(-1)
-#define MAP_NONE				0
-#define MAP_BUTTON				1
-
-static uint8	read_idx[2 /* ports */][2 /* per port */];
-static struct
-{
-	uint16				buttons;
-	uint16				turbos;
-	uint16				toggleturbo;
-	uint16				togglestick;
-	uint8				turbo_ct;
-}	joypad[8];
-
-static map<uint32, s9xcommand_t>	keymap;
-static int							turbo_time;
-static int							FLAG_LATCH = FALSE;
-static int							curcontrollers[2] = { NONE,    NONE };
-static int							newcontrollers[2] = { JOYPAD0, NONE };
-static char							buf[256];
+static s9xcommand_t keymap[MaxControlID + 1];
+static int			FLAG_LATCH = FALSE;
+static char			buf[256];
+static struct {
+	uint32	read_idx;
+	uint32	buttons;
+} joypads[2];
 
 // Note: these should be in asciibetical order!
 #define THE_COMMANDS \
@@ -57,20 +25,15 @@ static char							buf[256];
 	S(DecEmuTurbo), \
 	S(DecFrameRate), \
 	S(DecFrameTime), \
-	S(DecTurboSpeed), \
 	S(EmuTurbo), \
 	S(ExitEmu), \
 	S(IncEmuTurbo), \
 	S(IncFrameRate), \
 	S(IncFrameTime), \
-	S(IncTurboSpeed), \
 	S(LoadFreezeFile), \
 	S(Pause), \
-	S(QuickLoad000), \
-	S(QuickSave000), \
 	S(Reset), \
 	S(SaveFreezeFile), \
-	S(SeekToFrame), \
 	S(SoftReset), \
 	S(SoundChannel0), \
 	S(SoundChannel1), \
@@ -81,7 +44,6 @@ static char							buf[256];
 	S(SoundChannel6), \
 	S(SoundChannel7), \
 	S(SoundChannelsOn), \
-	S(SwapJoypads), \
 	S(ToggleBG0), \
 	S(ToggleBG1), \
 	S(ToggleBG2), \
@@ -110,49 +72,6 @@ static const char	*command_names[LAST_COMMAND + 1] =
 #undef S
 #undef THE_COMMANDS
 
-static int maptype (int t)
-{
-	switch (t)
-	{
-		case S9xNoMapping:
-			return (MAP_NONE);
-
-		case S9xButtonJoypad:
-		case S9xButtonCommand:
-		case S9xButtonPort:
-			return (MAP_BUTTON);
-
-		default:
-			return (MAP_UNKNOWN);
-	}
-}
-
-static bool strless (const char *a, const char *b)
-{
-	return (strcmp(a, b) < 0);
-}
-
-static int findstr (const char *needle, const char **haystack, int numstr)
-{
-	const char	**r;
-
-	r = lower_bound(haystack, haystack + numstr, needle, strless);
-	if (r >= haystack + numstr || strcmp(needle, *r))
-		return (-1);
-
-	return (r - haystack);
-}
-
-static const char * maptypename (int t)
-{
-	switch (t)
-	{
-		case MAP_NONE:		return ("unmapped");
-		case MAP_BUTTON:	return ("button");
-		default:			return ("unknown");
-	}
-}
-
 static void DisplayStateChange (const char *str, bool8 on)
 {
 	snprintf(buf, sizeof(buf), "%s: %s", str, on ? "on":"off");
@@ -161,93 +80,42 @@ static void DisplayStateChange (const char *str, bool8 on)
 
 void S9xControlsReset (void)
 {
-	for (int i = 0; i < 2; i++)
-		for (int j = 0; j < 2; j++)
-			read_idx[i][j]=0;
-
+	memset(&joypads, 0, sizeof(joypads));
 	FLAG_LATCH = FALSE;
-
-	curcontrollers[0] = newcontrollers[0];
-	curcontrollers[1] = newcontrollers[1];
 }
 
 void S9xUnmapAllControls (void)
 {
+	memset(&keymap, 0, sizeof(keymap));
 	S9xControlsReset();
-
-	keymap.clear();
-
-	for (int i = 0; i < 8; i++)
-	{
-		joypad[i].buttons  = 0;
-		joypad[i].turbos   = 0;
-		joypad[i].turbo_ct = 0;
-	}
-
-	turbo_time = 1;
-}
-
-void S9xSetController (int port, enum controllers controller, int8 id1, int8 id2, int8 id3, int8 id4)
-{
-	if (port < 0 || port > 1)
-		return;
-
-	switch (controller)
-	{
-		case CTL_NONE:
-			break;
-
-		case CTL_JOYPAD:
-			if (id1 < 0 || id1 > 7)
-				break;
-
-			newcontrollers[port] = JOYPAD0 + id1;
-			return;
-
-		default:
-			fprintf(stderr, "Unknown controller type %d\n", controller);
-			break;
-	}
-
-	newcontrollers[port] = NONE;
 }
 
 s9xcommand_t S9xGetCommandT (const char *name)
 {
-	s9xcommand_t	cmd;
+	s9xcommand_t	cmd = {
+		.type = S9xBadMapping,
+		.button_norpt = 0,
+		.command = 0,
+	};
 
-	memset(&cmd, 0, sizeof(cmd));
-	cmd.type         = S9xBadMapping;
-	cmd.multi_press  = 0;
-	cmd.button_norpt = 0;
-
+	if (!name)
+		cmd.type = S9xBadMapping;
+	else
 	if (!strcmp(name, "None"))
 		cmd.type = S9xNoMapping;
 	else
 	if (!strncmp(name, "Joypad", 6))
 	{
-		if (name[6] < '1' || name[6] > '8' || name[7] != ' ')
-			return (cmd);
+		if (((int)name[6] - '1') <= 0)
+			cmd.type = S9xButtonJoypad0;
+		else
+			cmd.type = S9xButtonJoypad1;
 
-		cmd.button.joypad.idx = name[6] - '1';
-		const char	*s = name + 8;
+		const char	*s = name + 7;
 		int i = 0;
 
-		if ((cmd.button.joypad.toggle = strncmp(s, "Toggle", 6) ? 0 : 1))	s += i = 6;
-		if ((cmd.button.joypad.sticky = strncmp(s, "Sticky", 6) ? 0 : 1))	s += i = 6;
-		if ((cmd.button.joypad.turbo  = strncmp(s, "Turbo",  5) ? 0 : 1))	s += i = 5;
-
-		if (cmd.button.joypad.toggle && !(cmd.button.joypad.sticky || cmd.button.joypad.turbo))
-			return (cmd);
-
-		if (i)
-		{
-			if (*s != ' ')
-				return (cmd);
+		while (s[0] == ' ')
 			s++;
-		}
-
-		i = 0;
 
 		if (!strncmp(s, "Up",     2))	{ i |= SNES_UP_MASK;     s += 2; if (*s == '+') s++; }
 		if (!strncmp(s, "Down",   4))	{ i |= SNES_DOWN_MASK;   s += 4; if (*s == '+') s++; }
@@ -267,18 +135,19 @@ s9xcommand_t S9xGetCommandT (const char *name)
 		if (i == 0 || *s != 0 || *(s - 1) == '+')
 			return (cmd);
 
-		cmd.button.joypad.buttons = i;
-
-		cmd.type = S9xButtonJoypad;
+		cmd.buttons = i;
 	}
 	else
 	{
-		int i = findstr(name, command_names, LAST_COMMAND);
-		if (i < 0)
-			return (cmd);
-
-		cmd.type = S9xButtonCommand;
-		cmd.button.command = i;
+		for (int i = 0; i < LAST_COMMAND; i++)
+		{
+			if (strcasecmp(command_names[i], name) == 0)
+			{
+				cmd.type = S9xButtonCommand;
+				cmd.command = i;
+				break;
+			}
+		}
 	}
 
 	return (cmd);
@@ -286,60 +155,55 @@ s9xcommand_t S9xGetCommandT (const char *name)
 
 s9xcommand_t S9xGetMapping (uint32 id)
 {
-	if (keymap.count(id) == 0)
+	if (id > MaxControlID)
 	{
-		s9xcommand_t	cmd;
-		cmd.type = S9xNoMapping;
-		return (cmd);
+		s9xcommand_t dummy = {0};
+		return dummy;
 	}
-	else
-		return (keymap[id]);
+
+	return (keymap[id]);
 }
 
-void S9xUnmapID (uint32 id)
+void S9xUnmapButton (uint32 id)
 {
-	keymap.erase(id);
-}
-
-bool S9xMapButtonT (uint32 id, const char *command, bool poll)
-{
-	return S9xMapButton(id, S9xGetCommandT(command), poll);
-}
-
-bool S9xMapButton (uint32 id, s9xcommand_t mapping, bool poll)
-{
-	int	t;
-
-	if (id == InvalidControlID)
+	if (id > MaxControlID)
 	{
-		fprintf(stderr, "Cannot map InvalidControlID\n");
+		printf("WARNING: Invalid Control ID %d, should be between 0 and %d\n", id, MaxControlID);
+		return;
+	}
+
+	memset(&keymap[id], 0, sizeof(s9xcommand_t));
+}
+
+bool S9xMapButtonT (uint32 id, const char *command)
+{
+	s9xcommand_t cmd = S9xGetCommandT(command);
+
+	if (cmd.type == S9xBadMapping)
+	{
+		printf("WARNING: Invalid command '%s'\n", command);
 		return (false);
 	}
 
-	t = maptype(mapping.type);
+	return S9xMapButton(id, cmd);
+}
 
-	if (t == MAP_NONE)
+bool S9xMapButton (uint32 id, s9xcommand_t mapping)
+{
+	if (id > MaxControlID)
 	{
-		S9xUnmapID(id);
+		printf("WARNING: Invalid Control ID %d, should be between 0 and %d\n", id, MaxControlID);
+		return (false);
+	}
+
+	if (mapping.type == S9xNoMapping)
+	{
+		S9xUnmapButton(id);
 		return (true);
 	}
 
-	if (t != MAP_BUTTON)
-		return (false);
-
-	t = maptype(S9xGetMapping(id).type);
-
-	if (t != MAP_NONE && t != MAP_BUTTON)
-		fprintf(stderr, "WARNING: Remapping ID 0x%08x from %s to button\n", id, maptypename(t));
-
-	t = -1;
-
-	if (poll)
-	{
-		//
-	}
-
-	S9xUnmapID(id);
+	if (keymap[id].type != S9xNoMapping)
+		fprintf(stderr, "WARNING: Remapping ID %d\n", id);
 
 	keymap[id] = mapping;
 
@@ -348,114 +212,56 @@ bool S9xMapButton (uint32 id, s9xcommand_t mapping, bool poll)
 
 void S9xReportButton (uint32 id, bool pressed)
 {
-	if (keymap.count(id) == 0)
+	if (id > MaxControlID)
 		return;
 
-	if (keymap[id].type == S9xNoMapping)
+	// skips the "already-pressed check" unless it's a command, as a hack to work around the following problem:
+	// FIXME: this makes the controls "stick" after loading a savestate while recording a movie and holding any button
+	if (keymap[id].type == S9xButtonCommand && keymap[id].button_norpt)
 		return;
-
-	if (maptype(keymap[id].type) != MAP_BUTTON)
-	{
-		fprintf(stderr, "ERROR: S9xReportButton called on %s ID 0x%08x\n", maptypename(maptype(keymap[id].type)), id);
-		return;
-	}
-
-	if (keymap[id].type == S9xButtonCommand)	// skips the "already-pressed check" unless it's a command, as a hack to work around the following problem:
-		if (keymap[id].button_norpt == pressed)	// FIXME: this makes the controls "stick" after loading a savestate while recording a movie and holding any button
-			return;
 
 	keymap[id].button_norpt = pressed;
 
-	S9xApplyCommand(keymap[id], pressed, 0);
+	S9xApplyCommand(keymap[id], pressed);
 }
 
-void S9xApplyCommand (s9xcommand_t cmd, int16 data1, int16 data2)
+void S9xApplyCommand (s9xcommand_t cmd, int data1)
 {
-	int	i;
-
 	switch (cmd.type)
 	{
 		case S9xNoMapping:
 			return;
 
-		case S9xButtonJoypad:
-			if (cmd.button.joypad.toggle)
-			{
-				if (!data1)
-					return;
-
-				uint16	r = cmd.button.joypad.buttons;
-
-				if (cmd.button.joypad.turbo)	joypad[cmd.button.joypad.idx].toggleturbo ^= r;
-				if (cmd.button.joypad.sticky)	joypad[cmd.button.joypad.idx].togglestick ^= r;
-			}
+		case S9xButtonJoypad0:
+			if (data1)
+				joypads[0].buttons |= cmd.buttons;
 			else
-			{
-				uint16	r, s, t, st;
+				joypads[0].buttons &= ~cmd.buttons;
+			return;
 
-				r = cmd.button.joypad.buttons;
-				st = r & joypad[cmd.button.joypad.idx].togglestick & joypad[cmd.button.joypad.idx].toggleturbo;
-				r ^= st;
-				t  = r & joypad[cmd.button.joypad.idx].toggleturbo;
-				r ^= t;
-				s  = r & joypad[cmd.button.joypad.idx].togglestick;
-				r ^= s;
-
-				if (cmd.button.joypad.turbo && cmd.button.joypad.sticky)
-				{
-					uint16	x = r; r = st; st = x;
-					x = s; s = t; t = x;
-				}
-				else
-				if (cmd.button.joypad.turbo)
-				{
-					uint16	x = r; r = t; t = x;
-					x = s; s = st; st = x;
-				}
-				else
-				if (cmd.button.joypad.sticky)
-				{
-					uint16	x = r; r = s; s = x;
-					x = t; t = st; st = x;
-				}
-
-				if (data1)
-				{
-					joypad[cmd.button.joypad.idx].buttons |= r;
-					joypad[cmd.button.joypad.idx].turbos  |= t;
-					joypad[cmd.button.joypad.idx].buttons ^= s;
-					joypad[cmd.button.joypad.idx].buttons &= ~(joypad[cmd.button.joypad.idx].turbos & st);
-					joypad[cmd.button.joypad.idx].turbos  ^= st;
-				}
-				else
-				{
-					joypad[cmd.button.joypad.idx].buttons &= ~r;
-					joypad[cmd.button.joypad.idx].buttons &= ~(joypad[cmd.button.joypad.idx].turbos & t);
-					joypad[cmd.button.joypad.idx].turbos  &= ~t;
-				}
-			}
+		case S9xButtonJoypad1:
+			if (data1)
+				joypads[1].buttons |= cmd.buttons;
+			else
+				joypads[1].buttons &= ~cmd.buttons;
 
 			return;
 
 		case S9xButtonCommand:
-			if (((enum command_numbers) cmd.button.command) >= LAST_COMMAND)
-			{
-				fprintf(stderr, "Unknown command %04x\n", cmd.button.command);
-				return;
-			}
-
 			if (!data1)
 			{
-				switch (i = cmd.button.command)
+				switch (cmd.command)
 				{
 					case EmuTurbo:
 						Settings.TurboMode = FALSE;
 						break;
+					default:
+						fprintf(stderr, "Unknown command %04x\n", cmd.command);
 				}
 			}
 			else
 			{
-				switch ((enum command_numbers) (i = cmd.button.command))
+				switch (cmd.command)
 				{
 					case ExitEmu:
 						S9xExit();
@@ -556,28 +362,10 @@ void S9xApplyCommand (s9xcommand_t cmd, int16 data1, int16 data2)
 						S9xSetInfoString(buf);
 						break;
 
-					case IncTurboSpeed:
-						if (turbo_time >= 120)
-							break;
-						turbo_time++;
-						sprintf(buf, "Turbo speed: %d", turbo_time);
-						S9xSetInfoString(buf);
-						break;
-
-					case DecTurboSpeed:
-						if (turbo_time <= 1)
-							break;
-						turbo_time--;
-						sprintf(buf, "Turbo speed: %d", turbo_time);
-						S9xSetInfoString(buf);
-						break;
-
-					case QuickLoad000:
 					case LoadFreezeFile:
 						// S9xUnfreezeGame(S9xChooseFilename(TRUE));
 						break;
 
-					case QuickSave000:
 					case SaveFreezeFile:
 						// S9xFreezeGame(S9xChooseFilename(FALSE));
 						break;
@@ -595,8 +383,8 @@ void S9xApplyCommand (s9xcommand_t cmd, int16 data1, int16 data2)
 					case SoundChannel5:
 					case SoundChannel6:
 					case SoundChannel7:
-						S9xToggleSoundChannel(i - SoundChannel0);
-						sprintf(buf, "Sound channel %d toggled", i - SoundChannel0);
+						S9xToggleSoundChannel((int)cmd.command - SoundChannel0);
+						sprintf(buf, "Sound channel %d toggled", (int)cmd.command - SoundChannel0);
 						S9xSetInfoString(buf);
 						break;
 
@@ -635,57 +423,14 @@ void S9xApplyCommand (s9xcommand_t cmd, int16 data1, int16 data2)
 						DisplayStateChange("Transparency effects", Settings.Transparency);
 						break;
 
-					case SwapJoypads:
-						if ((curcontrollers[0] != NONE && !(curcontrollers[0] >= JOYPAD0 && curcontrollers[0] <= JOYPAD7)))
-						{
-							S9xSetInfoString("Cannot swap pads: port 1 is not a joypad");
-							break;
-						}
-
-						if ((curcontrollers[1] != NONE && !(curcontrollers[1] >= JOYPAD0 && curcontrollers[1] <= JOYPAD7)))
-						{
-							S9xSetInfoString("Cannot swap pads: port 2 is not a joypad");
-							break;
-						}
-
-						newcontrollers[1] = curcontrollers[0];
-						newcontrollers[0] = curcontrollers[1];
-
-						strcpy(buf, "Swap pads: P1=");
-						i = 14;
-						if (newcontrollers[0] == NONE)
-						{
-							strcpy(buf + i, "<none>");
-							i += 6;
-						}
-						else
-						{
-							sprintf(buf + i, "Joypad%d", newcontrollers[0] - JOYPAD0 + 1);
-							i += 7;
-						}
-
-						strcpy(buf + i, " P2=");
-						i += 4;
-						if (newcontrollers[1] == NONE)
-							strcpy(buf + i, "<none>");
-						else
-							sprintf(buf + i, "Joypad%d", newcontrollers[1] - JOYPAD0 + 1);
-
-						S9xSetInfoString(buf);
-						break;
-
-					case SeekToFrame:
-						break;
-
 					case LAST_COMMAND:
 						break;
+
+					default:
+						fprintf(stderr, "Unknown command %04x\n", cmd.command);
 				}
 			}
 
-			return;
-
-		case S9xButtonPort:
-			S9xHandlePortCommand(cmd, data1, data2);
 			return;
 
 		default:
@@ -696,83 +441,38 @@ void S9xApplyCommand (s9xcommand_t cmd, int16 data1, int16 data2)
 
 void S9xSetJoypadLatch (bool latch)
 {
-	if (!latch && FLAG_LATCH)
-	{
-		// 1 written, 'plug in' new controllers now
-		curcontrollers[0] = newcontrollers[0];
-		curcontrollers[1] = newcontrollers[1];
-	}
-
 	if (latch && !FLAG_LATCH)
 	{
-		memset(read_idx, 0, sizeof(read_idx));
+		joypads[0].read_idx = 0;
+		joypads[1].read_idx = 0;
 	}
 
 	FLAG_LATCH = latch;
 }
 
-// prevent read_idx from overflowing (only latching resets it)
-// otherwise $4016/7 reads will start returning input data again
-static inline uint8 IncreaseReadIdxPost(uint8 &var)
-{
-	uint8 oldval = var;
-	if (var < 255)
-		var++;
-	return oldval;
-}
-
 uint8 S9xReadJOYSERn (int n)
 {
-	if (n > 1)
-		n -= 0x4016;
-
-	assert(n == 0 || n == 1);
+	n &= 1;
 
 	int bits = (OpenBus & ~3) | ((n == 1) ? 0x1c : 0);
-	int i = curcontrollers[n];
 
 	if (FLAG_LATCH)
 	{
-		switch (i)
-		{
-			case JOYPAD0:
-			case JOYPAD1:
-			case JOYPAD2:
-			case JOYPAD3:
-			case JOYPAD4:
-			case JOYPAD5:
-			case JOYPAD6:
-			case JOYPAD7:
-				return (bits | ((joypad[i - JOYPAD0].buttons & 0x8000) ? 1 : 0));
-
-			default:
-				return (bits);
-		}
+		return (bits | ((joypads[n].buttons >> 15) & 1));
 	}
 	else
 	{
-		switch (i)
-		{
-			case JOYPAD0:
-			case JOYPAD1:
-			case JOYPAD2:
-			case JOYPAD3:
-			case JOYPAD4:
-			case JOYPAD5:
-			case JOYPAD6:
-			case JOYPAD7:
-				if (read_idx[n][0] >= 16)
-				{
-					IncreaseReadIdxPost(read_idx[n][0]);
-					return (bits | 1);
-				}
-				else
-					return (bits | ((joypad[i - JOYPAD0].buttons & (0x8000 >> IncreaseReadIdxPost(read_idx[n][0]))) ? 1 : 0));
+		int read_idx = joypads[n].read_idx;
 
-			default:
-				IncreaseReadIdxPost(read_idx[n][0]);
-				return (bits);
-		}
+		// prevent read_idx from overflowing (only latching resets it)
+		// otherwise $4016/7 reads will start returning input data again
+		if (read_idx < 255)
+			joypads[n].read_idx++;
+
+		if (read_idx >= 16)
+			return (bits | 1);
+		else
+			return (bits | (joypads[n].buttons & (0x8000 >> joypads[n].read_idx) & 1));
 	}
 }
 
@@ -783,55 +483,8 @@ void S9xDoAutoJoypad (void)
 
 	for (int n = 0; n < 2; n++)
 	{
-		int i = curcontrollers[n];
-		switch (i)
-		{
-			case JOYPAD0:
-			case JOYPAD1:
-			case JOYPAD2:
-			case JOYPAD3:
-			case JOYPAD4:
-			case JOYPAD5:
-			case JOYPAD6:
-			case JOYPAD7:
-				read_idx[n][0] = 16;
-				WRITE_WORD(Memory.FillRAM + 0x4218 + n * 2, joypad[i - JOYPAD0].buttons);
-				WRITE_WORD(Memory.FillRAM + 0x421c + n * 2, 0);
-				break;
-
-			default:
-				WRITE_WORD(Memory.FillRAM + 0x4218 + n * 2, 0);
-				WRITE_WORD(Memory.FillRAM + 0x421c + n * 2, 0);
-				break;
-		}
-	}
-}
-
-void S9xControlEOF (void)
-{
-	for (int n = 0; n < 2; n++)
-	{
-		int i = curcontrollers[n];
-		switch (i)
-		{
-			case JOYPAD0:
-			case JOYPAD1:
-			case JOYPAD2:
-			case JOYPAD3:
-			case JOYPAD4:
-			case JOYPAD5:
-			case JOYPAD6:
-			case JOYPAD7:
-				if (++joypad[i - JOYPAD0].turbo_ct >= turbo_time)
-				{
-					joypad[i - JOYPAD0].turbo_ct = 0;
-					joypad[i - JOYPAD0].buttons ^= joypad[i - JOYPAD0].turbos;
-				}
-
-				break;
-
-			default:
-				break;
-		}
+		joypads[n].read_idx = 16;
+		WRITE_WORD(Memory.FillRAM + 0x4218 + n * 2, joypads[n].buttons);
+		WRITE_WORD(Memory.FillRAM + 0x421c + n * 2, 0);
 	}
 }

snes9x-go/components/snes9x/memory.cpp
@@ -4,7 +4,6 @@
    For further information, consult the LICENSE file in the root directory.
 \*****************************************************************************/
 
-#include <string>
 #include "snes9x.h"
 #include "memory.h"
 #include "apu/apu.h"

snes9x-go/components/snes9x/controls.h
@@ -8,109 +8,32 @@
 #define _CONTROLS_H_
 
 #define S9xNoMapping			0
-#define S9xButtonJoypad			1
-#define S9xButtonCommand		2
-
-// These are automatically kicked out to the S9xHandlePortCommand function.
-// If your port wants to define port-specific commands or whatever, use these values for the s9xcommand_t type field.
-
-#define S9xButtonPort			251
-
+#define S9xButtonJoypad0		1
+#define S9xButtonJoypad1		2
+#define S9xButtonCommand		3
 #define S9xBadMapping			255
-#define InvalidControlID		((uint32) -1)
+
+#define MaxControlID			63
 
 typedef struct
 {
 	uint8	type;
-	uint8	multi_press:2;
-	uint8	button_norpt:1;
-
+	uint8	button_norpt;
 	union
 	{
-		union
-		{
-			struct
-			{
-				uint8	idx:3;				// Pad number 0-7
-				uint8	toggle:1;			// If set, toggle turbo/sticky for the button
-				uint8	turbo:1;			// If set, be a 'turbo' button
-				uint8	sticky:1;			// If set, toggle button state (on/turbo or off) when pressed and do nothing on release
-				uint16	buttons;			// Which buttons to actuate. Use SNES_*_MASK constants from snes9x.h
-			}	joypad;
-
-			int32	multi_idx;
-			uint16	command;
-		}	button;
-
-		uint8	port[4];
+		uint16	buttons;			// Which buttons to actuate. Use SNES_*_MASK constants from snes9x.h
+		uint16	command;
 	};
 }	s9xcommand_t;
 
-// Starting out...
-
 void S9xUnmapAllControls (void);
-
-// Setting which controllers are plugged in.
-
-enum controllers
-{
-	CTL_NONE,		// all ids ignored
-	CTL_JOYPAD,		// use id1 to specify 0-7
-};
-
-void S9xSetController (int port, enum controllers controller, int8 id1, int8 id2, int8 id3, int8 id4); // port=0-1
-
-// Functions for translation s9xcommand_t's into strings, and vice versa.
-// free() the returned string after you're done with it.
-
 s9xcommand_t S9xGetCommandT (const char *name);
-
-// Generic mapping functions
-
 s9xcommand_t S9xGetMapping (uint32 id);
-void S9xUnmapID (uint32 id);
-
-// Button mapping functions.
-// If a button is mapped with poll=TRUE, then S9xPollButton will be called whenever snes9x feels a need for that mapping.
-// Otherwise, snes9x will assume you will call S9xReportButton() whenever the button state changes.
-// S9xMapButton() will fail and return FALSE if mapping.type isn't an S9xButton* type.
-
-bool S9xMapButton (uint32 id, s9xcommand_t mapping, bool poll);
-bool S9xMapButtonT (uint32 id, const char *command, bool poll = false);
+bool S9xMapButton (uint32 id, s9xcommand_t mapping);
+bool S9xMapButtonT (uint32 id, const char *command);
 void S9xReportButton (uint32 id, bool pressed);
-
-// Do whatever the s9xcommand_t says to do.
-// If cmd.type is a button type, data1 should be TRUE (non-0) or FALSE (0) to indicate whether the 'button' is pressed or released.
-// If cmd.type is an axis, data1 holds the deflection value.
-// If cmd.type is a pointer, data1 and data2 are the positions of the pointer.
-
-void S9xApplyCommand (s9xcommand_t cmd, int16 data1, int16 data2);
-
-//////////
-// These functions are called by snes9x into your port, so each port should implement them.
-
-// If something was mapped with poll=TRUE, these functions will be called when snes9x needs the button/axis/pointer state.
-// Fill in the reference options as appropriate.
-
-bool S9xPollButton (uint32 id, bool *pressed);
-
-// These are called when snes9x tries to apply a command with a S9x*Port type.
-// data1 and data2 are filled in like S9xApplyCommand.
-
-void S9xHandlePortCommand (s9xcommand_t cmd, int16 data1, int16 data2);
-
-// Called before already-read SNES joypad data is being used by the game if your port defines SNES_JOY_READ_CALLBACKS.
-
-#ifdef SNES_JOY_READ_CALLBACKS
-void S9xOnSNESPadRead (void);
-#endif
-
-// These are for your use.
-
-s9xcommand_t S9xGetPortCommandT (const char *name);
-char * S9xGetPortCommandName (s9xcommand_t command);
-void S9xSetupDefaultKeymap (void);
-bool8 S9xMapInput (const char *name, s9xcommand_t *cmd);
+void S9xUnmapButton (uint32 id);
+void S9xApplyCommand (s9xcommand_t cmd, int data);
 
 //////////
 // These functions are called from snes9x into this subsystem. No need to use them from a port.
@@ -126,9 +49,5 @@ void S9xSetJoypadLatch (bool latch);
 // Use when reading $4016/7 (JOYSER0 and JOYSER1).
 
 uint8 S9xReadJOYSERn (int n);
-
-// End-Of-Frame processing. Sets gun latch variables and tries to draw crosshairs
-
-void S9xControlEOF (void);
 
 #endif

snes9x-go/components/snes9x/gfx.cpp
@@ -1358,7 +1358,7 @@ static inline uint8 CalcWindowMask (int i, uint8 W1, uint8 W2)
 	return (0);
 }
 
-static inline void StoreWindowRegions (uint8 Mask, struct ClipData *Clip, int n_regions, int16 *windows, uint8 *drawing_modes, bool8 sub, bool8 StoreMode0 = FALSE)
+static inline void StoreWindowRegions (uint8 Mask, struct ClipData *Clip, int n_regions, int16 *windows, uint8 *drawing_modes, bool8 sub, bool8 StoreMode0)
 {
 	int	ct = 0;
 
@@ -1520,14 +1520,14 @@ static inline void ComputeClipWindows (void)
 		uint8	W = Settings.DisableGraphicWindows ? 0 : CalcWindowMask(j, W1, W2);
 
 		if (Memory.FillRAM[0x212e] & (1 << j))
-			StoreWindowRegions(W, &IPPU.Clip[0][j], n_regions, windows, drawing_modes, 0);
+			StoreWindowRegions(W, &IPPU.Clip[0][j], n_regions, windows, drawing_modes, 0, FALSE);
 		else
-			StoreWindowRegions(0, &IPPU.Clip[0][j], n_regions, windows, drawing_modes, 0);
+			StoreWindowRegions(0, &IPPU.Clip[0][j], n_regions, windows, drawing_modes, 0, FALSE);
 
 		if (Memory.FillRAM[0x212f] & (1 << j))
-			StoreWindowRegions(W, &IPPU.Clip[1][j], n_regions, windows, drawing_modes, 1);
+			StoreWindowRegions(W, &IPPU.Clip[1][j], n_regions, windows, drawing_modes, 1, FALSE);
 		else
-			StoreWindowRegions(0, &IPPU.Clip[1][j], n_regions, windows, drawing_modes, 1);
+			StoreWindowRegions(0, &IPPU.Clip[1][j], n_regions, windows, drawing_modes, 1, FALSE);
 	}
 }
 
@@ -1648,14 +1648,10 @@ void S9xEndScreenRefresh (void)
 	{
 		FLUSH_REDRAW();
 
-		S9xControlEOF();
-
 		S9xDisplayMessages(GFX.Screen, GFX.PPL, IPPU.RenderedScreenWidth, IPPU.RenderedScreenHeight, 1);
 
 		S9xDeinitUpdate(IPPU.RenderedScreenWidth, IPPU.RenderedScreenHeight);
 	}
-	else
-		S9xControlEOF();
 
 #ifdef DEBUGGER
 	if (CPU.Flags & FRAME_ADVANCE_FLAG)

snes9x-go/components/snes9x/ppu.cpp
@@ -1274,13 +1274,6 @@ uint8 S9xGetCPU (uint32 Address)
 {
 	if (Address < 0x4200)
 	{
-	#ifdef SNES_JOY_READ_CALLBACKS
-		if (Address == 0x4016 || Address == 0x4017)
-		{
-			S9xOnSNESPadRead();
-		}
-	#endif
-
 		switch (Address)
 		{
 			case 0x4016: // JOYSER0
@@ -1388,12 +1381,6 @@ uint8 S9xGetCPU (uint32 Address)
 			case 0x421d: // JOY3H
 			case 0x421e: // JOY4L
 			case 0x421f: // JOY4H
-			#ifdef SNES_JOY_READ_CALLBACKS
-				if (Memory.FillRAM[0x4200] & 1)
-				{
-					S9xOnSNESPadRead();
-				}
-			#endif
 				return (Memory.FillRAM[Address]);
 
 			default:

commit 8230d3506dec8c96e4479aab596dd7165a7a0357

SNES: Fixed controls in Super Star Wars that I broke in a45b2fbe

snes9x-go/components/snes9x/controls.cpp
@@ -461,17 +461,14 @@ uint8 S9xReadJOYSERn (int n)
 	}
 	else
 	{
-		int read_idx = joypads[n].read_idx;
-
-		// prevent read_idx from overflowing (only latching resets it)
-		// otherwise $4016/7 reads will start returning input data again
-		if (read_idx < 255)
-			joypads[n].read_idx++;
-
-		if (read_idx >= 16)
-			return (bits | 1);
+		if (joypads[n].read_idx < 16)
+		{
+			return (bits | ((joypads[n].buttons >> (15 - joypads[n].read_idx++)) & 1));
+		}
 		else
-			return (bits | (joypads[n].buttons & (0x8000 >> joypads[n].read_idx) & 1));
+		{
+			return (bits | 1);
+		}
 	}
 }
 

snes9x-go/components/snes9x/memory.cpp
@@ -161,6 +161,7 @@ static void map_space (uint32 bank_s, uint32 bank_e, uint32 addr_s, uint32 addr_
 		{
 			p = (c << 4) | (i >> 12);
 			Memory.ReadMap[p] = data;
+			Memory.WriteMap[p] = data;
 		}
 	}
 }
@@ -175,6 +176,7 @@ static void map_index (uint32 bank_s, uint32 bank_e, uint32 addr_s, uint32 addr_
 		{
 			p = (c << 4) | (i >> 12);
 			Memory.ReadMap[p] = (uint8 *) (pint) index;
+			Memory.WriteMap[p] = (uint8 *) (pint) index;
 		}
 	}
 }
@@ -201,18 +203,6 @@ static void Map_Finalize (void)
 	// Map Work RAM
 	map_space(0x7e, 0x7e, 0x0000, 0xffff, Memory.RAM);
 	map_space(0x7f, 0x7f, 0x0000, 0xffff, Memory.RAM + 0x10000);
-
-#if !RETRO_COMBINED_MEMORY_MAP
-	// Write protect ROM
-	memcpy((void *) Memory.WriteMap, (void *) Memory.ReadMap, sizeof(Memory.ReadMap));
-	for (int c = 0; c < MEMMAP_NUM_BLOCKS; c++)
-	{
-		if (Memory.WriteMap[c] >= Memory.ROM && Memory.WriteMap[c] <= (Memory.ROM + Memory.ROM_SIZE + 0x8000))
-		{
-			Memory.WriteMap[c] = (uint8 *) MAP_NONE;
-		}
-	}
-#endif
 }
 
 static void Map_DSP (void)