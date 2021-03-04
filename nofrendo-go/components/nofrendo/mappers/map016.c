25bc37941da0598584a9a8929a60af3f8093a98e
Alex Duchesne <alex@netvps.ca>
2021-03-04 14:24:44 -0500

    NES: Fixed mapper 16's nt mirroring not set correctly
    According to https://wiki.nesdev.com/w/index.php/INES_Mapper_016
    
    There are still other problems to work out in the mapper

--- nofrendo-go/components/nofrendo/mappers/map016.c
+++ nofrendo-go/components/nofrendo/mappers/map016.c
@@ -70,15 +70,15 @@ static void map16_write(uint32 address, uint8 value)
       case 0x9:
          switch (value & 3)
          {
-         case 0: ppu_setmirroring(PPU_MIRROR_HORI); break;
-         case 1: ppu_setmirroring(PPU_MIRROR_VERT); break;
+         case 0: ppu_setmirroring(PPU_MIRROR_VERT); break;
+         case 1: ppu_setmirroring(PPU_MIRROR_HORI); break;
          case 2: ppu_setmirroring(PPU_MIRROR_SCR0); break;
          case 3: ppu_setmirroring(PPU_MIRROR_SCR1); break;
          }
          break;
 
       case 0xA:
-         irq.enabled = (value & 1) ? true : false;
+         irq.enabled = (value & 1);
          break;
 
       case 0xB:
@@ -145,44 +145,3 @@ mapintf_t map16_intf =
    map16_memwrite, /* memory write structure */
    NULL /* external sound device */
 };
-
-/*
-** $Log: map016.c,v $
-** Revision 1.2  2001/04/27 14:37:11  neil
-** wheeee
-**
-** Revision 1.1  2001/04/27 12:54:40  neil
-** blah
-**
-** Revision 1.1.1.1  2001/04/27 07:03:54  neil
-** initial
-**
-** Revision 1.1  2000/10/24 12:19:33  matt
-** changed directory structure
-**
-** Revision 1.8  2000/10/22 19:17:46  matt
-** mapper cleanups galore
-**
-** Revision 1.7  2000/10/22 15:03:13  matt
-** simplified mirroring
-**
-** Revision 1.6  2000/10/21 19:33:38  matt
-** many more cleanups
-**
-** Revision 1.5  2000/10/10 13:58:16  matt
-** stroustrup squeezing his way in the door
-**
-** Revision 1.4  2000/08/16 02:50:11  matt
-** random mapper cleanups
-**
-** Revision 1.3  2000/07/15 23:52:20  matt
-** rounded out a bunch more mapper interfaces
-**
-** Revision 1.2  2000/07/11 05:03:49  matt
-** value masking isn't necessary for the banking routines
-**
-** Revision 1.1  2000/07/11 03:14:18  melanson
-** Initial commit for mappers 16, 34, and 231
-**
-**
-*/
