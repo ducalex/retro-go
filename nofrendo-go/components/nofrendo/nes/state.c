c75192a702984b183daa8e0af2f26db85b7c165d
Alex Duchesne <alex@netvps.ca>
2021-12-29 12:23:24 -0500

    NES: Added some documentation about save state format

--- nofrendo-go/components/nofrendo/nes/state.c
+++ nofrendo-go/components/nofrendo/nes/state.c
@@ -26,12 +26,44 @@
 #include "state.h"
 #include "cpu.h"
 
+/**
+ * Save file format:
+ *
+ * The file has an 8 byte header followed by a series of blocks.
+ * Integer values are big endian.
+ *
+ *    Header format:
+ *       magic (4 bytes), always "SNSS"
+ *       block count (4 bytes), int32be
+ *
+ *    Block format:
+ *       name (4 bytes)
+ *       block version (4 bytes), int32be
+ *       block length (4 bytes), int32be
+ *       data ("block length" bytes)
+ *
+ * In actuality, most blocks have a fixed size and all are at version 1.
+ * - BASR: 6449 bytes
+ * - INFO: 256 bytes
+ * - SOUN: 22 bytes
+ * - SRAM: 8193 bytes
+ * - VRAM: 8192 bytes
+ * - MPRD: 152 bytes
+ */
+
 typedef struct
 {
-    uint8  type[4];
-    uint32 blockVersion;
-    uint32 blockLength;
-} SnssBlockHeader;
+   uint8  magic[4];
+   uint32 blocks;
+} header_t;
+
+typedef struct
+{
+   uint8  name[4];
+   uint32 version;
+   uint32 length;
+   uint8  data[];
+} block_t;
 
 #define _fread(buffer, size) {                       \
    if (fread(buffer, size, 1, file) != 1)            \
@@ -67,14 +99,10 @@ typedef struct
 
 int state_save(const char* fn)
 {
+   uint32 numberOfBlocks = 0;
    uint8 buffer[512];
-   uint8 numberOfBlocks = 0;
-   nes_t *machine;
+   nes_t *machine = nes_getptr();
    FILE *file;
-   uint16 i, temp;
-
-   /* get the pointer to our NES machine context */
-   machine = nes_getptr();
 
    if (!(file = fopen(fn, "wb")))
    {
@@ -91,83 +119,51 @@ int state_save(const char* fn)
 
    MESSAGE_INFO("  - Saving base block\n");
 
-   // SnssBlockHeader
-   _fwrite("BASR\x00\x00\x00\x01\x00\x00\x19\x31", 12);
-   numberOfBlocks++;
-
    buffer[0] = machine->cpu->a_reg;
    buffer[1] = machine->cpu->x_reg;
    buffer[2] = machine->cpu->y_reg;
    buffer[3] = machine->cpu->p_reg;
    buffer[4] = machine->cpu->s_reg;
-   temp = swap16(machine->cpu->pc_reg);
-   buffer[5] = ((uint8*)&temp)[0];
-   buffer[6] = ((uint8*)&temp)[1];
+   buffer[5] = machine->cpu->pc_reg / 256;
+   buffer[6] = machine->cpu->pc_reg % 256;
    buffer[7] = machine->ppu->ctrl0;
    buffer[8] = machine->ppu->ctrl1;
 
+   _fwrite("BASR\x00\x00\x00\x01\x00\x00\x19\x31", 12);
    _fwrite(&buffer, 9);
-
    _fwrite(machine->mem->ram, 0x800);
    _fwrite(machine->ppu->oam, 0x100);
    _fwrite(machine->ppu->nametab, 0x1000);
 
    /* Mask off priority color bits */
-   for (i = 0; i < 32; i++)
-   {
+   for (int i = 0; i < 32; i++)
       buffer[i] = machine->ppu->palette[i] & 0x3F;
-   }
 
    buffer[32] = machine->ppu->nt1;
    buffer[33] = machine->ppu->nt2;
    buffer[34] = machine->ppu->nt3;
    buffer[35] = machine->ppu->nt4;
-   temp = swap16(machine->ppu->vaddr);
-   buffer[36] = ((uint8*)&temp)[0];
-   buffer[37] = ((uint8*)&temp)[1];
+   buffer[36] = machine->ppu->vaddr / 256;
+   buffer[37] = machine->ppu->vaddr % 256;
    buffer[38] = machine->ppu->oam_addr;
    buffer[39] = machine->ppu->tile_xofs;
 
    _fwrite(&buffer, 40);
+   numberOfBlocks++;
 
 
    /****************************************************/
 
-   if (machine->cart->chr_ram_banks > 0)
-   {
-      MESSAGE_INFO("  - Saving VRAM block\n");
+   MESSAGE_INFO("  - Saving info block\n");
 
-      // SnssBlockHeader
-      _fwrite("VRAM\x00\x00\x00\x01\x00\x00\x20\x00", 12);
-      numberOfBlocks++;
-
-      _fwrite(machine->cart->chr_ram, 0x2000);
-   }
-
-
-   /****************************************************/
-
-   if (machine->cart->prg_ram_banks > 0)
-   {
-      MESSAGE_INFO("  - Saving SRAM block\n");
-
-      // Byte 13 = SRAM enabled (unused)
-      // Length is always $2001
-      // SnssBlockHeader
-      _fwrite("SRAM\x00\x00\x00\x01\x00\x00\x20\x01\x01", 13);
-      numberOfBlocks++;
-
-      _fwrite(machine->cart->prg_ram, 0x2000);
-   }
+   _fwrite("INFO\x00\x00\x00\x01\x00\x00\x01\x00", 12);
+   _fwrite(&buffer, 0x100);
+   numberOfBlocks++;
 
 
    /****************************************************/
 
    MESSAGE_INFO("  - Saving sound block\n");
-
-   // SnssBlockHeader
-   _fwrite("SOUN\x00\x00\x00\x01\x00\x00\x00\x16", 12);
-   numberOfBlocks++;
 
    buffer[0x00] = machine->apu->rectangle[0].regs[0];
    buffer[0x01] = machine->apu->rectangle[0].regs[1];
@@ -189,7 +185,35 @@ int state_save(const char* fn)
    buffer[0x13] = machine->apu->dmc.regs[3];
    buffer[0x15] = machine->apu->control_reg;
 
+   _fwrite("SOUN\x00\x00\x00\x01\x00\x00\x00\x16", 12);
    _fwrite(&buffer, 0x16);
+   numberOfBlocks++;
+
+
+   /****************************************************/
+
+   if (machine->cart->chr_ram_banks > 0)
+   {
+      MESSAGE_INFO("  - Saving VRAM block\n");
+
+      _fwrite("VRAM\x00\x00\x00\x01\x00\x00\x20\x00", 12);
+      _fwrite(machine->cart->chr_ram, 0x2000);
+      numberOfBlocks++;
+   }
+
+
+   /****************************************************/
+
+   if (machine->cart->prg_ram_banks > 0)
+   {
+      MESSAGE_INFO("  - Saving SRAM block\n");
+
+      // Byte 0 = SRAM enabled (unused)
+      // Length is always $2001
+      _fwrite("SRAM\x00\x00\x00\x01\x00\x00\x20\x01\x01", 13);
+      _fwrite(machine->cart->prg_ram, 0x2000);
+      numberOfBlocks++;
+   }
 
 
    /****************************************************/
@@ -198,24 +222,16 @@ int state_save(const char* fn)
    {
       MESSAGE_INFO("  - Saving mapper block\n");
 
-      // SnssBlockHeader
-      _fwrite("MPRD\x00\x00\x00\x01\x00\x00\x00\x98", 12);
-      numberOfBlocks++;
-
-      uint8 state[0x80];
-      uint16 temp;
-
-      /* TODO: snss spec should be updated, using 4kB ROM pages.. */
-      for (i = 0; i < 4; i++)
+      for (int i = 0; i < 4; i++)
       {
-         temp = swap16((mem_getpage((i + 4) * 4) - machine->cart->prg_rom) >> 13);
+         uint16 temp = swap16((mem_getpage((i + 4) * 4) - machine->cart->prg_rom) >> 13);
          buffer[(i * 2) + 0] = ((uint8 *) &temp)[0];
          buffer[(i * 2) + 1] = ((uint8 *) &temp)[1];
       }
 
-      for (i = 0; i < 8; i++)
+      for (int i = 0; i < 8; i++)
       {
-         temp = (machine->cart->chr_rom_banks) ?
+         uint16 temp = (machine->cart->chr_rom_banks) ?
             ((ppu_getpage(i) - machine->cart->chr_rom + (i * 0x400)) >> 10) : (i);
          temp = swap16(temp);
          buffer[8 + (i * 2) + 0] = ((uint8 *) &temp)[0];
@@ -224,19 +240,21 @@ int state_save(const char* fn)
 
       if (machine->mapper->get_state)
       {
-         machine->mapper->get_state(state);
-         memcpy(buffer + 0x18, state, 0x80);
+         machine->mapper->get_state(buffer + 0x18);
       }
 
+      _fwrite("MPRD\x00\x00\x00\x01\x00\x00\x00\x98", 12);
       _fwrite(&buffer, 0x98);
+      numberOfBlocks++;
    }
 
 
    /****************************************************/
 
    // Update number of blocks
-   fseek(file, 7, SEEK_SET);
-   fwrite(&numberOfBlocks, 1, 1, file);
+   fseek(file, 4, SEEK_SET);
+   numberOfBlocks = swap32(numberOfBlocks);
+   _fwrite(&numberOfBlocks, 4);
 
    fclose(file);
 
@@ -250,19 +268,17 @@ _error:
    return -1;
 }
 
+
 int state_load(const char* fn)
 {
    uint8 buffer[512];
-   FILE *file;
-   nes_t *machine;
-   int blk, i;
-
    uint32 numberOfBlocks;
    uint32 blockVersion;
    uint32 blockLength;
    uint32 nextBlock = 8;
 
-   machine = nes_getptr();
+   nes_t *machine = nes_getptr();
+   FILE *file;
 
    if (!(file = fopen(fn, "rb")))
    {
@@ -282,7 +298,7 @@ int state_load(const char* fn)
 
    MESSAGE_INFO("state_load: file '%s' opened, blocks=%d.\n", fn, numberOfBlocks);
 
-   for (blk = 0; blk < numberOfBlocks; blk++)
+   for (int blk = 0; blk < numberOfBlocks; blk++)
    {
       fseek(file, nextBlock, SEEK_SET);
       _fread(buffer, 12);
@@ -317,7 +333,7 @@ int state_load(const char* fn)
          _fread(machine->ppu->palette, 0x20);
 
          /* TODO: argh, this is to handle nofrendo's filthy sprite priority method */
-         for (i = 0; i < 8; i++)
+         for (int i = 0; i < 8; i++)
             machine->ppu->palette[i << 2] = machine->ppu->palette[0] | 0x80; // BG_TRANS;
 
          _fread(buffer, 8);
@@ -346,7 +362,7 @@ int state_load(const char* fn)
 
          if (machine->cart->chr_ram_banks < (blockLength / ROM_CHR_BANK_SIZE))
          {
-            MESSAGE_ERROR("Invalid CHR-RAM size!\n");
+            MESSAGE_ERROR("Invalid block size!\n");
             continue;
          }
 
@@ -362,7 +378,7 @@ int state_load(const char* fn)
 
          if (machine->cart->prg_ram_banks < ((blockLength-1) / ROM_PRG_BANK_SIZE))
          {
-            MESSAGE_ERROR("Invalid PRG-RAM size!\n");
+            MESSAGE_ERROR("Invalid block size!\n");
             continue;
          }
 
@@ -377,19 +393,19 @@ int state_load(const char* fn)
       {
          MESSAGE_INFO("  - Found mapper block\n");
 
-         _fread(buffer, blockLength);
+         _fread(buffer, 0x98);
 
-         for (i = 0; i < 4; i++)
+         for (int i = 0; i < 4; i++)
             mmc_bankrom(8, 0x8000 + (i * 0x2000), swap16(((uint16*)buffer)[i]));
 
          if (machine->cart->chr_rom_banks)
          {
-            for (i = 0; i < 8; i++)
+            for (int i = 0; i < 8; i++)
                mmc_bankvrom(1, i * 0x400, swap16(((uint16*)buffer)[4 + i]));
          }
          else if (machine->cart->chr_ram)
          {
-            for (i = 0; i < 8; i++)
+            for (int i = 0; i < 8; i++)
                ppu_setpage(1, i, machine->cart->chr_ram);
          }
 
@@ -408,8 +424,20 @@ int state_load(const char* fn)
 
          apu_reset();
 
-         for (i = 0; i < 0x16; i++)
+         for (int i = 0; i < 0x16; i++)
             apu_write(0x4000 + i, buffer[i]);
+      }
+
+
+      /****************************************************/
+
+      else if (memcmp(buffer, "INFO", 4) == 0)
+      {
+         MESSAGE_INFO("  - Found info block\n");
+
+         _fread(buffer, 0x100);
+
+         // We don't currently do anything with it, it's just to help report bugs to me :)
       }
 
 
