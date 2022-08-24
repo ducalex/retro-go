f9f645469427742e9201471147f044835190bcaf
Alex Duchesne <alex@netvps.ca>
2022-08-24 16:29:11 -0400

    rg_tool: There's no need for a list of targets in the file
    It scans targets/*.h either way.

--- rg_tool.py
+++ rg_tool.py
@@ -17,7 +17,7 @@ try:
 except:
     pass
 
-TARGETS = ["odroid-go", "mrgc-g32", "qtpy-gamer", "retro-esp32"]
+TARGETS = ["odroid-go"] # We just need to specify the default, the others are discovered below
 for t in glob.glob("components/retro-go/targets/*.h"):
     TARGETS.append(os.path.basename(t)[0:-2])
 
