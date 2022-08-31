b05968607b8ec077d9825e7178cd89e6807cde6b
Alex Duchesne <alex@netvps.ca>
2022-08-31 07:29:56 -0400

    rg_i2c: Fixed some issues
    - Ignore repeated calls to rg_i2c_init instead of re-init/possible crash
    - Removed implicit initialization, rg_i2c_init must always be called
    - Fixed possible memory leak on i2c errors

--- components/retro-go/rg_i2c.c
+++ components/retro-go/rg_i2c.c
@@ -1,20 +1,17 @@
-#include <esp_system.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_err.h>
 #include "rg_system.h"
 #include "rg_i2c.h"
 
 #if defined(RG_GPIO_I2C_SDA) && defined(RG_GPIO_I2C_SCL)
-    #include <freertos/FreeRTOS.h>
     #include <driver/i2c.h>
     #define USE_I2C_DRIVER
 #endif
 
 #define TRY(x) if ((err = (x)) != ESP_OK) { goto fail; }
 
-static enum {
-    INIT_PENDING,
-    INIT_FAILED,
-    INIT_OK
-} i2cStatus = INIT_PENDING;
+static bool i2c_initialized = false;
 
 
 bool rg_i2c_init(void)
@@ -23,56 +20,49 @@ bool rg_i2c_init(void)
     const i2c_config_t i2c_config = {
         .mode = I2C_MODE_MASTER,
         .sda_io_num = RG_GPIO_I2C_SDA,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
         .scl_io_num = RG_GPIO_I2C_SCL,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
         .scl_pullup_en = GPIO_PULLUP_ENABLE,
         .master.clk_speed = 400000,
     };
     esp_err_t err = ESP_FAIL;
 
+    if (i2c_initialized)
+        return true;
+
     TRY(i2c_param_config(I2C_NUM_0, &i2c_config));
     TRY(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
     RG_LOGI("I2C driver ready (SDA:%d SCL:%d).\n", i2c_config.sda_io_num, i2c_config.scl_io_num);
-    i2cStatus = INIT_OK;
+    i2c_initialized = true;
     return true;
 fail:
     RG_LOGE("Failed to initialize I2C driver. err=0x%x\n", err);
 #else
-    RG_LOGE("I2C driver is not available on this device.\n");
+    RG_LOGE("I2C is not available on this device.\n");
 #endif
-    i2cStatus = INIT_FAILED;
+    i2c_initialized = false;
     return false;
 }
 
 bool rg_i2c_deinit(void)
 {
 #ifdef USE_I2C_DRIVER
-    if (i2cStatus != INIT_PENDING)
-    {
-        if (i2c_driver_delete(I2C_NUM_0) == ESP_OK)
-            RG_LOGI("I2C driver terminated.\n");
-    }
+    if (i2c_initialized && i2c_driver_delete(I2C_NUM_0) == ESP_OK)
+        RG_LOGI("I2C driver terminated.\n");
 #endif
-    i2cStatus = INIT_PENDING;
+    i2c_initialized = false;
     return true;
-}
-
-bool rg_i2c_available(void)
-{
-    if (i2cStatus == INIT_PENDING)
-        return rg_i2c_init();
-    return i2cStatus == INIT_OK;
 }
 
 bool rg_i2c_read(uint8_t addr, int reg, void *read_data, size_t read_len)
 {
-    esp_err_t err = ESP_FAIL;
-
-    if (!rg_i2c_available())
-        goto fail;
-
 #ifdef USE_I2C_DRIVER
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    esp_err_t err = ESP_FAIL;
+
+    if (!cmd || !i2c_initialized)
+        goto fail;
+
     if (reg >= 0)
     {
         TRY(i2c_master_start(cmd));
@@ -86,22 +76,22 @@ bool rg_i2c_read(uint8_t addr, int reg, void *read_data, size_t read_len)
     TRY(i2c_master_cmd_begin(I2C_NUM_0, cmd, pdMS_TO_TICKS(500)));
     i2c_cmd_link_delete(cmd);
     return true;
-#endif
-
 fail:
-    RG_LOGE("Read from 0x%02x failed. reg=%d, err=0x%x\n", addr, reg, err);
+    i2c_cmd_link_delete(cmd);
+    RG_LOGE("Read from 0x%02x failed. reg=%d, err=0x%x, init=%d\n", addr, reg, err, i2c_initialized);
+#endif
     return false;
 }
 
 bool rg_i2c_write(uint8_t addr, int reg, const void *write_data, size_t write_len)
 {
-    esp_err_t err = ESP_FAIL;
-
-    if (!rg_i2c_available())
-        goto fail;
-
 #ifdef USE_I2C_DRIVER
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    esp_err_t err = ESP_FAIL;
+
+    if (!cmd || !i2c_initialized)
+        goto fail;
+
     TRY(i2c_master_start(cmd));
     TRY(i2c_master_write_byte(cmd, (addr << 1) | I2C_MASTER_WRITE, true));
     if (reg >= 0)
@@ -113,20 +103,19 @@ bool rg_i2c_write(uint8_t addr, int reg, const void *write_data, size_t write_le
     TRY(i2c_master_cmd_begin(I2C_NUM_0, cmd, pdMS_TO_TICKS(500)));
     i2c_cmd_link_delete(cmd);
     return true;
-#endif
-
 fail:
-    RG_LOGE("Write to 0x%02x failed. reg=%d, err=0x%x\n", addr, reg, err);
+    RG_LOGE("Write to 0x%02x failed. reg=%d, err=0x%x, init=%d\n", addr, reg, err, i2c_initialized);
+#endif
     return false;
-}
-
-bool rg_i2c_write_byte(uint8_t addr, uint8_t reg, uint8_t value)
-{
-    return rg_i2c_write(addr, reg, &value, 1);
 }
 
 uint8_t rg_i2c_read_byte(uint8_t addr, uint8_t reg)
 {
     uint8_t value = 0;
     return rg_i2c_read(addr, reg, &value, 1) ? value : 0;
+}
+
+bool rg_i2c_write_byte(uint8_t addr, uint8_t reg, uint8_t value)
+{
+    return rg_i2c_write(addr, reg, &value, 1);
 }
--- components/retro-go/rg_i2c.h
+++ components/retro-go/rg_i2c.h
@@ -6,9 +6,16 @@
 
 bool rg_i2c_init(void);
 bool rg_i2c_deinit(void);
-bool rg_i2c_available(void);
 bool rg_i2c_read(uint8_t addr, int reg, void *read_data, size_t read_len);
 bool rg_i2c_write(uint8_t addr, int reg, const void *write_data, size_t write_len);
-bool rg_i2c_write_byte(uint8_t addr, uint8_t reg, uint8_t value);
 uint8_t rg_i2c_read_byte(uint8_t addr, uint8_t reg);
+bool rg_i2c_write_byte(uint8_t addr, uint8_t reg, uint8_t value);
 
+// GPIO extender
+bool rg_i2c_gpio_init(void);
+bool rg_i2c_gpio_deinit(void);
+bool rg_i2c_gpio_set_direction(int pin, int mode);
+int  rg_i2c_gpio_get_level(int pin);
+bool rg_i2c_gpio_set_level(int pin, int level);
+uint8_t rg_i2c_gpio_read_port(int port);
+bool rg_i2c_gpio_write_port(int port, uint8_t value);
