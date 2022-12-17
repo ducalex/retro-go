e3e86a32cbc2fcb2a823f64a89c07620d6edcbcc
Alex Duchesne <alex@netvps.ca>
2022-12-16 22:27:38 -0500

    Launcher: Reduce memory usage in the updater
    The HTTP client seems to need some internal heap (for mbedtls) so getting rid of the JSON (>90KB) ASAP helps with stability.

--- launcher/main/updater.c
+++ launcher/main/updater.c
@@ -12,6 +12,20 @@
 #define DOWNLOAD_LOCATION RG_STORAGE_ROOT "/espgbc/firmware"
 #endif
 
+typedef struct
+{
+    char name[32];
+    char url[256];
+} asset_t;
+
+typedef struct
+{
+    char name[32];
+    char date[32];
+    asset_t *assets;
+    size_t assets_count;
+} release_t;
+
 static int download_file(const char *url, const char *filename)
 {
     RG_ASSERT(url && filename, "bad param");
@@ -90,77 +104,89 @@ cleanup:
     return json;
 }
 
-void updater_show_dialog(void)
+static rg_gui_event_t view_release_cb(rg_gui_option_t *option, rg_gui_event_t event)
 {
-    cJSON *releases = fetch_json(GITHUB_RELEASES_URL);
-    size_t releases_length = RG_MIN(cJSON_GetArraySize(releases), 16);
-
-    rg_gui_option_t options[releases_length + 1];
-    rg_gui_option_t *opt = options;
-
-    if (releases_length == 0)
+    if (event == RG_DIALOG_ENTER)
     {
-        rg_gui_alert("Error", "Received empty list");
-        goto cleanup;
-    }
+        release_t *release = (release_t *)option->arg;
 
-    for (int i = 0; i < releases_length; i++)
-    {
-        cJSON *release = cJSON_GetArrayItem(releases, i);
-        *opt++ = (rg_gui_option_t){i, cJSON_GetStringValue(cJSON_GetObjectItem(release, "name")), NULL, 1, NULL};
-    }
-    *opt++ = (rg_gui_option_t)RG_DIALOG_END;
-
-    int sel = rg_gui_dialog("Available releases", options, 0);
-    if (sel != RG_DIALOG_CANCELLED)
-    {
-        cJSON *release = cJSON_GetArrayItem(releases, sel);
-        char *release_name = cJSON_GetStringValue(cJSON_GetObjectItem(release, "name"));
-        char *release_date = cJSON_GetStringValue(cJSON_GetObjectItem(release, "published_at"));
-        cJSON *assets = cJSON_GetObjectItem(release, "assets");
-        size_t assets_length = cJSON_GetArraySize(assets);
-
-        rg_gui_option_t options[assets_length + 4];
+        rg_gui_option_t options[release->assets_count + 4];
         rg_gui_option_t *opt = options;
 
-        *opt++ = (rg_gui_option_t){0, "Date", release_date, -1, NULL};
-        *opt++ = (rg_gui_option_t){0, "Downloads:", NULL, -1, NULL};
+        *opt++ = (rg_gui_option_t){0, "Date", release->date, -1, NULL};
+        *opt++ = (rg_gui_option_t){0, "Files:", NULL, -1, NULL};
 
-        for (int i = 0; i < assets_length; i++)
-        {
-            cJSON *asset = cJSON_GetArrayItem(assets, i);
-            *opt++ = (rg_gui_option_t){i, cJSON_GetStringValue(cJSON_GetObjectItem(asset, "name")), NULL, 1, NULL};
-        }
+        for (int i = 0; i < release->assets_count; i++)
+            *opt++ = (rg_gui_option_t){i, release->assets[i].name, NULL, 1, NULL};
         *opt++ = (rg_gui_option_t)RG_DIALOG_END;
 
-        int sel = rg_gui_dialog(release_name, options, -1);
+        int sel = rg_gui_dialog(release->name, options, -1);
         if (sel != RG_DIALOG_CANCELLED)
         {
-            cJSON *asset = cJSON_GetArrayItem(assets, sel);
-            char *asset_name = strdup(cJSON_GetStringValue(cJSON_GetObjectItem(asset, "name")));
-            char *asset_url = strdup(cJSON_GetStringValue(cJSON_GetObjectItem(asset, "browser_download_url")));
-            char *dest_path = malloc(RG_PATH_MAX);
-            snprintf(dest_path, RG_PATH_MAX, "%s/%s", DOWNLOAD_LOCATION, asset_name);
+            char dest_path[RG_PATH_MAX];
+            snprintf(dest_path, RG_PATH_MAX, "%s/%s", DOWNLOAD_LOCATION, release->assets[sel].name);
 
-            // This is a bit ugly but at this point we need to free some internal heap because mbedtls
-            // doesn't seem to want to use SPIRAM :(
-            cJSON_Delete(releases);
-
-            // Clean the screen, make it easier to see progress
+            int ret = download_file(release->assets[sel].url, dest_path);
             gui_redraw();
 
-            if (download_file(asset_url, dest_path) == 0)
-                rg_gui_alert("Download complete!", dest_path);
-            else
-                rg_gui_alert("Download failed!", dest_path);
+            if (ret != 0)
+                rg_gui_alert("Download failed!", "...");
+            else if (rg_gui_confirm("Download complete!", "Reboot to flash?", true))
+                rg_system_switch_app(RG_APP_FACTORY, RG_APP_FACTORY, NULL, 0);
+        }
+        gui_redraw();
+    }
 
-            free(asset_name);
-            free(asset_url);
-            free(dest_path);
-            return;
+    return RG_DIALOG_VOID;
+}
+
+void updater_show_dialog(void)
+{
+    cJSON *releases_json = fetch_json(GITHUB_RELEASES_URL);
+    size_t releases_count = RG_MIN(cJSON_GetArraySize(releases_json), 20);
+
+    release_t *releases = calloc(releases_count, sizeof(release_t));
+    for (int i = 0; i < releases_count; ++i)
+    {
+        cJSON *release_json = cJSON_GetArrayItem(releases_json, i);
+        cJSON *assets_json = cJSON_GetObjectItem(release_json, "assets");
+        size_t assets_count = cJSON_GetArraySize(assets_json);
+
+        snprintf(releases[i].name, 32, "%s", cJSON_GetStringValue(cJSON_GetObjectItem(release_json, "name")));
+        snprintf(releases[i].date, 32, "%s", cJSON_GetStringValue(cJSON_GetObjectItem(release_json, "published_at")));
+        releases[i].assets = calloc(assets_count, sizeof(asset_t));
+        releases[i].assets_count = assets_count;
+
+        for (int j = 0; j < assets_count; ++j)
+        {
+            cJSON *asset_json = cJSON_GetArrayItem(assets_json, j);
+            asset_t *asset = &releases[i].assets[j];
+            snprintf(asset->name, 32, "%s", cJSON_GetStringValue(cJSON_GetObjectItem(asset_json, "name")));
+            snprintf(asset->url, 256, "%s", cJSON_GetStringValue(cJSON_GetObjectItem(asset_json, "browser_download_url")));
         }
     }
 
-cleanup:
-    cJSON_Delete(releases);
+    cJSON_Delete(releases_json);
+
+    gui_redraw();
+
+    if (releases_count > 0)
+    {
+        rg_gui_option_t options[releases_count + 1];
+        rg_gui_option_t *opt = options;
+
+        for (int i = 0; i < releases_count; ++i)
+            *opt++ = (rg_gui_option_t){(intptr_t)&releases[i], releases[i].name, NULL, 1, &view_release_cb};
+        *opt++ = (rg_gui_option_t)RG_DIALOG_END;
+
+        rg_gui_dialog("Available Releases", options, 0);
+    }
+    else
+    {
+        rg_gui_alert("Available Releases", "Received empty list!");
+    }
+
+    for (int i = 0; i < releases_count; ++i)
+        free(releases[i].assets);
+    free(releases);
 }
