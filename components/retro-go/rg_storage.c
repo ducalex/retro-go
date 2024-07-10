bb1c11fe2d8379444f699ea19424c75b27b59200
Tom Van Braeckel <tomvanbraeckel@gmail.com>
2024-07-10 16:31:08 +0200

    Improve comment
    Technically, an SD card is also flash memory, just external.
    So it makes sense to add an explicit "Internal" modifier to the word "Flash".

--- components/retro-go/rg_storage.c
+++ components/retro-go/rg_storage.c
@@ -168,7 +168,7 @@ void rg_storage_init(void)
 
 #endif
 
-#if ( RG_STORAGE_DRIVER == 4 || defined(RG_STORAGE_INTERNAL_FLASH_FALLBACK) ) && ESP_PLATFORM // SPI Flash
+#if ( RG_STORAGE_DRIVER == 4 || defined(RG_STORAGE_INTERNAL_FLASH_FALLBACK) ) && ESP_PLATFORM // SPI Internal Flash
 
     if (error_code) // only if no previous storage was successfully mounted already
     {
