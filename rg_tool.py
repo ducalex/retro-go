cbdbc89be86a3ee628227c09a523bf16e33cab01
Alex Duchesne <ducalex007@gmail.com>
2024-07-29 20:45:39 -0400

    rg_tool: fatsize can be a string

--- rg_tool.py
+++ rg_tool.py
@@ -85,7 +85,7 @@ def build_image(apps, device_type, img_format="esp32", fatsize=0):
 
     if fatsize:
         # Use "vfs" label, same as MicroPython, in case the storage is to be shared with a MicroPython install
-        table_csv.append("vfs, data, fat, %d, %d" % (len(image_data), fatsize))
+        table_csv.append("vfs, data, fat, %d, %s" % (len(image_data), fatsize))
 
     print("Generating partition table...")
     with open("partitions.csv", "w") as f:
