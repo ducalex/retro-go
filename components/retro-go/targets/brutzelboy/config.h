a220b6c3b06516597da3f6f857289e84228f212f
theBrutzler <34459192+theBrutzler@users.noreply.github.com>
2025-01-15 21:05:02 +0100

    add brutzelboy

---
+++ components/retro-go/targets/brutzelboy/config.h
@@ -0,0 +1,96 @@
+// REF: https://wiki.odroid.com/odroid_go/odroid_go
+
+// Target definition
+#define RG_TARGET_NAME             "brutzelboy"
+
+// Storage
+#define RG_STORAGE_DRIVER           1                   // 0 = Host, 1 = SDSPI, 2 = SDMMC, 3 = USB, 4 = Flash
+#define RG_STORAGE_HOST             SPI3_HOST   // Used by driver 1 and 2
+#define RG_STORAGE_SPEED            SDMMC_FREQ_DEFAULT  // Used by driver 1 and 2
+#define RG_STORAGE_ROOT             "/sd"               // Storage mount point
+
+// Audio
+#define RG_AUDIO_USE_INT_DAC        0   // 0 = Disable, 1 = GPIO25, 2 = GPIO26, 3 = Both
+#define RG_AUDIO_USE_EXT_DAC        1   // 0 = Disable, 1 = Enable
+
+// Video
+#define RG_SCREEN_DRIVER            0   // 0 = ILI9341
+#define RG_SCREEN_HOST              SPI2_HOST
+#define RG_SCREEN_SPEED             SPI_MASTER_FREQ_40M // SPI_MASTER_FREQ_40M
+#define RG_SCREEN_WIDTH             320 //320
+#define RG_SCREEN_HEIGHT            240
+#define RG_SCREEN_ROTATE            0
+#define RG_SCREEN_MARGIN_TOP        0
+#define RG_SCREEN_MARGIN_BOTTOM     0
+#define RG_SCREEN_MARGIN_LEFT       0
+#define RG_SCREEN_MARGIN_RIGHT      36
+#define RG_SCREEN_INIT()                                                                                         \
+    ILI9341_CMD(0xCF, 0x00, 0xc3, 0x30);                                                                         \
+    ILI9341_CMD(0xED, 0x64, 0x03, 0x12, 0x81);                                                                   \
+    ILI9341_CMD(0xE8, 0x85, 0x00, 0x78);                                                                         \
+    ILI9341_CMD(0xCB, 0x39, 0x2c, 0x00, 0x34, 0x02);                                                             \
+    ILI9341_CMD(0xF7, 0x20);                                                                                     \
+    ILI9341_CMD(0xEA, 0x00, 0x00);                                                                               \
+    ILI9341_CMD(0xC0, 0x1B);                 /* Power control   //VRH[5:0] */                                    \
+    ILI9341_CMD(0xC1, 0x12);                 /* Power control   //SAP[2:0];BT[3:0] */                            \
+    ILI9341_CMD(0xC5, 0x32, 0x3C);           /* VCM control */                                                   \
+    ILI9341_CMD(0xC7, 0x91);                 /* VCM control2 */                                                  \
+    ILI9341_CMD(0x36, (0x20 | 0x80 | 0x08)); /* Memory Access Control  thebrutzler hat ((0x60|0x08)^0xC0))*/     \
+    ILI9341_CMD(0xB1, 0x00, 0x10);           /* Frame Rate Control (1B=70, 1F=61, 10=119) */                     \
+    ILI9341_CMD(0xB6, 0x0A, 0xA2);           /* Display Function Control */                                      \
+    ILI9341_CMD(0xF6, 0x01, 0x30);                                                                               \
+    ILI9341_CMD(0xF2, 0x00); /* 3Gamma Function Disable */                                                       \
+    ILI9341_CMD(0x26, 0x01); /* Gamma curve selected */                                                          \
+    ILI9341_CMD(0xE0, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, 0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09, 0x00); \
+    ILI9341_CMD(0xE1, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, 0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36, 0x0F);
+
+
+// Input
+#define RG_GAMEPAD_DRIVER           7   // 1 = ODROID-GO, 2 = Serial, 3 = I2C, 7 = Like ODROID but all pull up pins
+#define RG_GAMEPAD_HAS_MENU_BTN     1
+#define RG_GAMEPAD_HAS_OPTION_BTN   1
+// 3072  1024
+#define RG_GAMEPAD_ADC1_MAP {\
+    {RG_KEY_UP,    ADC1_CHANNEL_6, ADC_ATTEN_DB_11, 1851, 4096},\
+    {RG_KEY_DOWN,  ADC1_CHANNEL_6, ADC_ATTEN_DB_11, 1500, 1820},\
+    {RG_KEY_LEFT,  ADC1_CHANNEL_5, ADC_ATTEN_DB_11, 1851, 4096},\
+    {RG_KEY_RIGHT, ADC1_CHANNEL_5, ADC_ATTEN_DB_11, 1500, 1820},\
+}
+// GPIO6 GPIO7
+#define RG_GAMEPAD_GPIO_MAP {\
+    {RG_KEY_SELECT, GPIO_NUM_16, GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_START,  GPIO_NUM_17, GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_MENU,   GPIO_NUM_18, GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_OPTION, GPIO_NUM_8,  GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_A,      GPIO_NUM_15, GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_B,      GPIO_NUM_5,  GPIO_PULLUP_ONLY, 0},\
+}
+
+// Battery
+#define RG_BATTERY_DRIVER           1
+#define RG_BATTERY_ADC_CHANNEL      ADC1_CHANNEL_3
+#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.7f - 2000.f) / (2700.f - 2000.f) * 100.f)
+#define RG_BATTERY_CALC_VOLTAGE(raw) ((raw) * 2.7f * 0.001f)
+
+// Status LED
+#define RG_GPIO_LED                 GPIO_NUM_38
+
+// SPI Display (back up working)
+#define RG_GPIO_LCD_MISO            GPIO_NUM_NC
+#define RG_GPIO_LCD_MOSI            GPIO_NUM_12
+#define RG_GPIO_LCD_CLK             GPIO_NUM_48
+#define RG_GPIO_LCD_CS              GPIO_NUM_NC
+#define RG_GPIO_LCD_DC              GPIO_NUM_47
+#define RG_GPIO_LCD_BCKL            GPIO_NUM_39
+#define RG_GPIO_LCD_RST             GPIO_NUM_3
+
+#define RG_GPIO_SDSPI_MISO          GPIO_NUM_9
+#define RG_GPIO_SDSPI_MOSI          GPIO_NUM_11
+#define RG_GPIO_SDSPI_CLK           GPIO_NUM_13
+#define RG_GPIO_SDSPI_CS            GPIO_NUM_10
+
+// External I2S DAC
+#define RG_GPIO_SND_I2S_BCK         41
+#define RG_GPIO_SND_I2S_WS          42
+#define RG_GPIO_SND_I2S_DATA        40
+// #define RG_GPIO_SND_AMP_ENABLE      18
