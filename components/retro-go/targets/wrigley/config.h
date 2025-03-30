f9c2526e1e77703da4bc56c1019336301ada37dc
DynaMight1124 <dynamight@gmail.com>
2025-03-30 17:36:51 +0100

    Update config.h

--- components/retro-go/targets/wrigley/config.h
+++ components/retro-go/targets/wrigley/config.h
@@ -18,7 +18,7 @@
 #define RG_SCREEN_HOST              SPI2_HOST
 #define RG_SCREEN_SPEED             SPI_MASTER_FREQ_40M
 #define RG_SCREEN_BACKLIGHT         1
-#define RG_SCREEN_WIDTH             300 // screen is technically a 280x240 but it works!
+#define RG_SCREEN_WIDTH             300 // screen is technically 280x240 but it works!
 #define RG_SCREEN_HEIGHT            240
 #define RG_SCREEN_ROTATE            0
 #define RG_SCREEN_MARGIN_TOP        0
