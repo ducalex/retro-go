40bc20743bc8a208908d5c2acc9992e2ea84abde
DynaMight1124 <dynamight@gmail.com>
2025-07-06 22:49:09 +0100

    Amend Null Nano ADC control values

--- components/retro-go/targets/nullnano/config.h
+++ components/retro-go/targets/nullnano/config.h
@@ -23,15 +23,6 @@
 #define RG_SCREEN_ROTATE            0
 #define RG_SCREEN_VISIBLE_AREA      {20, 0, 0, 0} // Left, Top, Right, Bottom 
 #define RG_SCREEN_SAFE_AREA         {20, 0, 20, 0} // Left, Top, Right, Bottom
-
-#define ST7789_MADCTL 0x36 // Memory Access Control
-#define ST7789_MADCTL_MY 0x80
-#define ST7789_MADCTL_MX 0x40
-#define ST7789_MADCTL_MV 0x20
-#define ST7789_MADCTL_ML 0x10
-#define ST7789_MADCTL_RGB 0x00
-#define ST7789_MADCTL_BGR 0x08
-
 #define RG_SCREEN_INIT()                                                                                         \
     ILI9341_CMD(0xCF, 0x00, 0xc3, 0x30);                                                                         \
     ILI9341_CMD(0xED, 0x64, 0x03, 0x12, 0x81);                                                                   \
@@ -43,11 +34,11 @@
     ILI9341_CMD(0xC1, 0x12);                 /* Power control   //SAP[2:0];BT[3:0] */                            \
     ILI9341_CMD(0xC5, 0x32, 0x3C);           /* VCM control */                                                   \
     ILI9341_CMD(0xC7, 0x91);                 /* VCM control2 */                                                  \
-    ILI9341_CMD(ST7789_MADCTL, (ST7789_MADCTL_BGR));           				 			 \
+    ILI9341_CMD(0x36, 0x08);                 /* Memory Access Control  (BGR) */                                  \
     ILI9341_CMD(0xB1, 0x00, 0x10);           /* Frame Rate Control (1B=70, 1F=61, 10=119) */                     \
     ILI9341_CMD(0xB6, 0x0A, 0xA2);           /* Display Function Control */                                      \
     ILI9341_CMD(0xF6, 0x01, 0x30);                                                                               \
-    ILI9341_CMD(0xF2, 0x00); /* 3Gamma Function Disable */                                                       \
+    ILI9341_CMD(0xF2, 0x00);                 /* 3Gamma Function Disable */                                       \
     ILI9341_CMD(0xE0, 0xD0, 0x00, 0x05, 0x0E, 0x15, 0x0D, 0x37, 0x43, 0x47, 0x09, 0x15, 0x12, 0x16, 0x19);       \
     ILI9341_CMD(0xE1, 0xD0, 0x00, 0x05, 0x0D, 0x0C, 0x06, 0x2D, 0x44, 0x40, 0x0E, 0x1C, 0x18, 0x16, 0x19);       \
 
@@ -55,18 +46,18 @@
 // Input
 // Refer to rg_input.h to see all available RG_KEY_* and RG_GAMEPAD_*_MAP types
 #define RG_GAMEPAD_ADC_MAP {\
-    {RG_KEY_UP,    ADC_UNIT_1, ADC_CHANNEL_7, ADC_ATTEN_DB_11, 2048, 4096},\
-    {RG_KEY_DOWN,  ADC_UNIT_1, ADC_CHANNEL_7, ADC_ATTEN_DB_11, 1024, 2047},\
-    {RG_KEY_LEFT,  ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 2048, 4096},\
-    {RG_KEY_RIGHT, ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 1024, 2047},\
+    {RG_KEY_UP,    ADC_UNIT_1, ADC_CHANNEL_7, ADC_ATTEN_DB_11, 1950, 4096},\
+    {RG_KEY_DOWN,  ADC_UNIT_1, ADC_CHANNEL_7, ADC_ATTEN_DB_11, 1024, 1900},\
+    {RG_KEY_LEFT,  ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 1950, 4096},\
+    {RG_KEY_RIGHT, ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 1024, 1900},\
 }
 #define RG_GAMEPAD_GPIO_MAP {\
-    {RG_KEY_SELECT, GPIO_NUM_27, GPIO_PULLUP_ONLY, 0},\
-    {RG_KEY_START,  GPIO_NUM_39, GPIO_FLOATING,    0},\
-    {RG_KEY_MENU,   GPIO_NUM_13, GPIO_PULLUP_ONLY, 0},\
-    {RG_KEY_OPTION, GPIO_NUM_0,  GPIO_FLOATING,    0},\
-    {RG_KEY_A,      GPIO_NUM_32, GPIO_PULLUP_ONLY, 0},\
-    {RG_KEY_B,      GPIO_NUM_33, GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_SELECT, .num = GPIO_NUM_27, .pullup = 1, .level = 0},\
+    {RG_KEY_START,  .num = GPIO_NUM_39, .pullup = 0, .level = 0},\
+    {RG_KEY_MENU,   .num = GPIO_NUM_13, .pullup = 1, .level = 0},\
+    {RG_KEY_OPTION, .num = GPIO_NUM_0,  .pullup = 0, .level = 0},\
+    {RG_KEY_A,      .num = GPIO_NUM_32, .pullup = 1, .level = 0},\
+    {RG_KEY_B,      .num = GPIO_NUM_33, .pullup = 1, .level = 0},\
 }
 
 // Battery
