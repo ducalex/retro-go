f357884a289e496a6e4a32ecc4e3f8b726a66c41
Alex Duchesne <ducalex007@gmail.com>
2025-07-26 15:54:46 -0400

    Regenerated fonts with font_converter.py
    I'm still tweaking font_converter.py but the result is pretty close to the previous fonts. In some cases the line height is increased because of diatrics unfortunately...

--- components/retro-go/fonts/fonts.h
+++ components/retro-go/fonts/fonts.h
@@ -13,8 +13,8 @@
 extern const rg_font_t font_basic8x8;
 extern const rg_font_t font_DejaVu12;
 extern const rg_font_t font_DejaVu15;
-extern const rg_font_t font_VeraBold12;
-extern const rg_font_t font_VeraBold15;
+extern const rg_font_t font_VeraBold11;
+extern const rg_font_t font_VeraBold14;
 
 enum {
     RG_FONT_BASIC_8,
@@ -22,8 +22,8 @@ enum {
     RG_FONT_BASIC_16,
     RG_FONT_DEJAVU_12,
     RG_FONT_DEJAVU_15,
-    RG_FONT_VERA_12,
-    RG_FONT_VERA_15,
+    RG_FONT_VERA_11,
+    RG_FONT_VERA_14,
     RG_FONT_MAX,
 };
 
@@ -33,6 +33,6 @@ static const rg_font_t *fonts[RG_FONT_MAX] = {
     &font_basic8x8,
     &font_DejaVu12,
     &font_DejaVu15,
-    &font_VeraBold12,
-    &font_VeraBold15,
+    &font_VeraBold11,
+    &font_VeraBold14,
 };
