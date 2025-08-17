f15d46a7251e02d372e420b7a5455162d028df11
Raphael Texier <raphaeltexier@aol.com>
2025-08-17 19:05:46 +0200

    esp32-p4: updated documentation

--- components/retro-go/targets/esp32-p4/config.h
+++ components/retro-go/targets/esp32-p4/config.h
@@ -3,28 +3,75 @@
  * command to build: python rg_tool.py --target esp32-p4 build-img --no-networking
 */
 
-// Target definition
+/****************************************************************************
+ * Target definition for ESP32-P4 Dev-Board                                 *
+ ****************************************************************************/
 #define RG_TARGET_NAME             "ESP32-P4"
 
-// Storage
-#define RG_STORAGE_ROOT               "/sd"
-#define RG_STORAGE_SDMMC_HOST         1
-#define RG_STORAGE_SDMMC_SPEED        SDMMC_FREQ_HIGHSPEED
 
-// Audio
+/****************************************************************************
+ * Status LED                                                               *
+ ****************************************************************************/
+// #define RG_LED_DRIVER               1   // 1 = GPIO
+// #define RG_GPIO_LED                 GPIO_NUM_NC
+// #define RG_GPIO_LED_INVERT          // Uncomment if the LED is active LOW
+
+
+/****************************************************************************
+ * I2C / GPIO Extender                                                      *
+ ****************************************************************************/
+// #define RG_I2C_GPIO_DRIVER          0   // 1 = AW9523, 2 = PCF9539, 3 = MCP23017, 4 = PCF8575
+// #define RG_I2C_GPIO_ADDR            0x00
+// #define RG_GPIO_I2C_SDA             GPIO_NUM_15
+// #define RG_GPIO_I2C_SCL             GPIO_NUM_4
+
+
+/****************************************************************************
+ * Storage                                                                  *
+ ****************************************************************************/
+#define RG_STORAGE_ROOT             "/sd"
+// #define RG_STORAGE_SDSPI_HOST       SPI2_HOST
+// #define RG_STORAGE_SDSPI_SPEED      SDMMC_FREQ_DEFAULT
+// #define RG_GPIO_SDSPI_MISO          GPIO_NUM_19
+// #define RG_GPIO_SDSPI_MOSI          GPIO_NUM_23
+// #define RG_GPIO_SDSPI_CLK           GPIO_NUM_18
+// #define RG_GPIO_SDSPI_CS            GPIO_NUM_22
+#define RG_STORAGE_SDMMC_HOST       SDMMC_HOST_SLOT_1
+#define RG_STORAGE_SDMMC_SPEED      SDMMC_FREQ_HIGHSPEED
+#define RG_GPIO_SDMMC_CLK           GPIO_NUM_43
+#define RG_GPIO_SDMMC_CMD	        GPIO_NUM_44
+#define RG_GPIO_SDMMC_D0	        GPIO_NUM_39
+#define RG_GPIO_SDMMC_D1	        GPIO_NUM_40
+#define RG_GPIO_SDMMC_D2	        GPIO_NUM_41
+#define RG_GPIO_SDMMC_D3	        GPIO_NUM_42
+// #define RG_STORAGE_FLASH_PARTITION  "vfs"
+
+
+/****************************************************************************
+ * Audio                                                                    *
+ ****************************************************************************/
 #define RG_AUDIO_USE_INT_DAC        0   // 0 = Disable, 1 = GPIO25, 2 = GPIO26, 3 = Both
 #define RG_AUDIO_USE_EXT_DAC        1   // 0 = Disable, 1 = Enable
+#define RG_GPIO_SND_I2S_BCK         GPIO_NUM_48
+#define RG_GPIO_SND_I2S_WS          GPIO_NUM_49
+#define RG_GPIO_SND_I2S_DATA        GPIO_NUM_46
+#define RG_GPIO_SND_AMP_ENABLE      GPIO_NUM_47
+// #define RG_GPIO_SND_AMP_ENABLE_INVERT // Uncomment if the mute = HIGH
 
-// Video
+
+/****************************************************************************
+ * Video                                                                    *
+ ****************************************************************************/
 #define RG_SCREEN_DRIVER            0   // 0 = ILI9341/ST7789
 #define RG_SCREEN_HOST              SPI3_HOST
-#define RG_SCREEN_SPEED             SPI_MASTER_FREQ_80M
+#define RG_SCREEN_SPEED             SPI_MASTER_FREQ_80M // SPI_MASTER_FREQ_40M
 #define RG_SCREEN_BACKLIGHT         0
 #define RG_SCREEN_WIDTH             320
 #define RG_SCREEN_HEIGHT            240
 #define RG_SCREEN_ROTATE            0
 #define RG_SCREEN_VISIBLE_AREA      {0, 0, 0, 0}  // Left, Top, Right, Bottom
 #define RG_SCREEN_SAFE_AREA         {0, 0, 0, 0}  // Left, Top, Right, Bottom
+#define RG_SCREEN_PARTIAL_UPDATES   1
 #define RG_SCREEN_INIT()                                                                                     \
 ILI9341_CMD(0xCF, 0x00, 0xc3, 0x30);                                                                         \
 ILI9341_CMD(0xED, 0x64, 0x03, 0x12, 0x81);                                                                   \
@@ -45,6 +92,20 @@ ILI9341_CMD(0x26, 0x01); /* Gamma curve selected */                             
 ILI9341_CMD(0xE0, 0xD0, 0x00, 0x02, 0x07, 0x0a, 0x28, 0x32, 0x44, 0x42, 0x06, 0x0e, 0x12, 0x14, 0x17);       \
 ILI9341_CMD(0xE1, 0xD0, 0x00, 0x02, 0x07, 0x0a, 0x28, 0x31, 0x54, 0x47, 0x0E, 0x1C, 0x17, 0x1b, 0x1e);       
 
+//#define RG_GPIO_LCD_MISO            GPIO_NUM_NC
+#define RG_GPIO_LCD_MOSI            GPIO_NUM_22
+#define RG_GPIO_LCD_CLK             GPIO_NUM_23
+#define RG_GPIO_LCD_CS              GPIO_NUM_24
+#define RG_GPIO_LCD_DC              GPIO_NUM_25
+#define RG_GPIO_LCD_RST             GPIO_NUM_26
+// #define RG_GPIO_LCD_BCKL         GPIO_NUM_NC
+// #define RG_GPIO_LCD_BCKL_INVERT  // Uncomment if the LED is active LOW
+
+
+/****************************************************************************
+ * Input                                                                    *
+ ****************************************************************************/
+// Refer to rg_input.h to see all available RG_KEY_* and RG_GAMEPAD_*_MAP types
 #define RG_GAMEPAD_GPIO_MAP {\
     {RG_KEY_LEFT,   .num = GPIO_NUM_13, .pullup = 1, .level = 0},\
     {RG_KEY_RIGHT,  .num = GPIO_NUM_14, .pullup = 1, .level = 0},\
@@ -61,28 +122,32 @@ ILI9341_CMD(0xE1, 0xD0, 0x00, 0x02, 0x07, 0x0a, 0x28, 0x31, 0x54, 0x47, 0x0E, 0x
     {RG_KEY_R,      .num = GPIO_NUM_9,  .pullup = 1, .level = 0},\
 }
 
-#define RG_RECOVERY_BTN RG_KEY_MENU // Keep this button pressed to open the recovery menu
 
-// SPI Display
-#define RG_GPIO_LCD_MISO    GPIO_NUM_NC
-#define RG_GPIO_LCD_MOSI    GPIO_NUM_22
-#define RG_GPIO_LCD_CLK     GPIO_NUM_23
-#define RG_GPIO_LCD_CS      GPIO_NUM_24
-#define RG_GPIO_LCD_DC      GPIO_NUM_25
-#define RG_GPIO_LCD_RST     GPIO_NUM_26
-#define RG_GPIO_LCD_BCKL    GPIO_NUM_27
+/****************************************************************************
+ * Battery                                                                  *
+ ****************************************************************************/
+// #define RG_BATTERY_DRIVER            1   // 1 = ADC, 2 = MRGC
+// #define RG_BATTERY_ADC_UNIT          ADC_UNIT_1
+// #define RG_BATTERY_ADC_CHANNEL       ADC_CHANNEL_0
+// #define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 3500.f) / (4200.f - 3500.f) * 100.f)
+// #define RG_BATTERY_CALC_VOLTAGE(raw) ((raw) * 2.f * 0.001f)
 
-// SDMMC SD Card
-// We use the default pins for SDMMC on the ESP32-P4 ie:
-#define RG_GPIO_SDMMC_CLK    GPIO_NUM_43
-#define RG_GPIO_SDMMC_CMD	 GPIO_NUM_44
-#define RG_GPIO_SDMMC_D0	 GPIO_NUM_39
-#define RG_GPIO_SDMMC_D1	 GPIO_NUM_40
-#define RG_GPIO_SDMMC_D2	 GPIO_NUM_41
-#define RG_GPIO_SDMMC_D3	 GPIO_NUM_42
 
-// External I2S DAC
-#define RG_GPIO_SND_I2S_BCK         GPIO_NUM_48
-#define RG_GPIO_SND_I2S_WS          GPIO_NUM_49
-#define RG_GPIO_SND_I2S_DATA        GPIO_NUM_46
-#define RG_GPIO_SND_AMP_ENABLE      GPIO_NUM_47
+/****************************************************************************
+ * Updater                                                                  *
+ ****************************************************************************/
+// #define RG_UPDATER_ENABLE               1
+// #define RG_UPDATER_APPLICATION          RG_APP_FACTORY
+// #define RG_UPDATER_DOWNLOAD_LOCATION    RG_STORAGE_ROOT "/odroid/firmware"
+
+
+
+/****************************************************************************
+ * Miscellaneous                                                            *
+ ****************************************************************************/
+#define RG_RECOVERY_BTN                 RG_KEY_MENU // Keep this button pressed to open the recovery menu
+
+#define RG_CUSTOM_PLATFORM_INIT() \
+    /* Arbitrary code executed very early during retro-go init */
+
+// See components/retro-go/config.h for more things you can define here!
