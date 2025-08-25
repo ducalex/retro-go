a89980d93de5ed4724c16506390983e30e1575bd
ioiotor <1279870549@qq.com>
2025-08-25 09:17:03 +0800

    Font: Add Fusion Pixel 12px monospaced font Added the fusion-pixel-12px-monospaced-zh_hans font and the converted font, which includes the first-level Chinese characters of the Chinese National Standard and the characters used in the Chinese translation.

--- components/retro-go/fonts/fonts.h
+++ components/retro-go/fonts/fonts.h
@@ -10,6 +10,7 @@ extern const rg_font_t font_DejaVu12;
 extern const rg_font_t font_DejaVu15;
 extern const rg_font_t font_VeraBold11;
 extern const rg_font_t font_VeraBold14;
+extern const rg_font_t font_FusionPixel;
 
 enum {
     RG_FONT_BASIC_8,
@@ -19,6 +20,7 @@ enum {
     RG_FONT_DEJAVU_15,
     RG_FONT_VERA_11,
     RG_FONT_VERA_14,
+    RG_FONT_FUSIONPIXEL_12,
     RG_FONT_MAX,
 };
 
@@ -30,4 +32,5 @@ static const rg_font_t *fonts[RG_FONT_MAX] = {
     &font_DejaVu15,
     &font_VeraBold11,
     &font_VeraBold14,
+    &font_FusionPixel,
 };
