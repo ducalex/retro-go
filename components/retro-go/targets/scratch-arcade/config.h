a7054b787e3afdf1843e3acadf9171bdf47a7b34
贺小帅 <hxsjs@qq.com>
2025-09-27 23:02:47 +0800

    New device: Scratch Arcade.

---
+++ components/retro-go/targets/scratch-arcade/config.h
@@ -0,0 +1,94 @@
+// Target definition
+#define RG_TARGET_NAME             "SCRATCH-ARCADE"
+
+// Storage
+#define RG_STORAGE_ROOT             "/vfs"
+#define RG_STORAGE_FLASH_PARTITION  "vfs"
+
+// Audio
+#define RG_AUDIO_USE_INT_DAC        0   // 0 = Disable
+#define RG_AUDIO_USE_EXT_DAC        1   // 0 = Disable, 1 = Enable
+
+// Video
+#define RG_SCREEN_DRIVER            0   // 0 = ST7789
+#define RG_SCREEN_HOST              SPI2_HOST
+#define RG_SCREEN_SPEED             SPI_MASTER_FREQ_40M // SPI_MASTER_FREQ_80M
+#define RG_SCREEN_BACKLIGHT         1
+#define RG_SCREEN_WIDTH             320
+#define RG_SCREEN_HEIGHT            240
+#define RG_SCREEN_ROTATE            0
+#define RG_SCREEN_VISIBLE_AREA      {0, 0, 0, 0} // Left, Top, Right, Bottom 
+#define RG_SCREEN_SAFE_AREA         {0, 0, 0, 0} // Left, Top, Right, Bottom
+
+#define ST7789_MADCTL 0x36 // Memory Access Control
+#define ST7789_MADCTL_MV 0x20
+#define ST7789_MADCTL_RGB 0x00
+#define ST7789_MADCTL_BGR 0x08
+
+#define RG_SCREEN_INIT()                                                                                         \
+    ILI9341_CMD(0xCF, 0x00, 0xc3, 0x30);                                                                         \
+    ILI9341_CMD(0xED, 0x64, 0x03, 0x12, 0x81);                                                                   \
+    ILI9341_CMD(0xE8, 0x85, 0x00, 0x78);                                                                         \
+    ILI9341_CMD(0xCB, 0x39, 0x2c, 0x00, 0x34, 0x02);                                                             \
+    ILI9341_CMD(0xF7, 0x20);                                                                                     \
+    ILI9341_CMD(0xEA, 0x00, 0x00);                                                                               \
+    ILI9341_CMD(0xC0, 0x1B);                 /* Power control   //VRH[5:0] */                                    \
+    ILI9341_CMD(0xC1, 0x12);                 /* Power control   //SAP[2:0];BT[3:0] */                            \
+    ILI9341_CMD(0xC5, 0x32, 0x3C);           /* VCM control */                                                   \
+    ILI9341_CMD(0xC7, 0x91);                 /* VCM control2 */                                                  \
+    ILI9341_CMD(ST7789_MADCTL, (ST7789_MADCTL_BGR));                                          \
+    ILI9341_CMD(0xB1, 0x00, 0x10);           /* Frame Rate Control (1B=70, 1F=61, 10=119) */                     \
+    ILI9341_CMD(0xB6, 0x0A, 0xA2);           /* Display Function Control */                                      \
+    ILI9341_CMD(0xF6, 0x01, 0x30);                                                                               \
+    ILI9341_CMD(0xF2, 0x00); /* 3Gamma Function Disable */                                                       \
+    ILI9341_CMD(0xE0, 0xD0, 0x00, 0x05, 0x0E, 0x15, 0x0D, 0x37, 0x43, 0x47, 0x09, 0x15, 0x12, 0x16, 0x19);       \
+    ILI9341_CMD(0xE1, 0xD0, 0x00, 0x05, 0x0D, 0x0C, 0x06, 0x2D, 0x44, 0x40, 0x0E, 0x1C, 0x18, 0x16, 0x19);
+
+
+// Input
+#define RG_GAMEPAD_ADC_MAP {\
+    {RG_KEY_UP,    ADC_UNIT_1, ADC_CHANNEL_7, ADC_ATTEN_DB_11, 3072, 4096},\
+    {RG_KEY_RIGHT, ADC_UNIT_1, ADC_CHANNEL_5, ADC_ATTEN_DB_11, 0,    1023},\
+    {RG_KEY_DOWN,  ADC_UNIT_1, ADC_CHANNEL_7, ADC_ATTEN_DB_11, 0,    1023},\
+    {RG_KEY_LEFT,  ADC_UNIT_1, ADC_CHANNEL_5, ADC_ATTEN_DB_11, 3072, 4096},\
+}
+
+// KEY Y GPIO4 being held down...
+#define RG_GAMEPAD_GPIO_MAP {\
+    {RG_KEY_START,  .num = GPIO_NUM_0,  .pullup = 1, .level = 0},\
+    {RG_KEY_A,      .num = GPIO_NUM_5, .pullup = 1, .level = 0},\
+    {RG_KEY_B,      .num = GPIO_NUM_39,  .pullup = 1, .level = 0},\
+    {RG_KEY_OPTION, .num = GPIO_NUM_9,  .pullup = 1, .level = 0},\
+}
+
+#define RG_GAMEPAD_VIRT_MAP {\
+    {RG_KEY_MENU,   .src = RG_KEY_OPTION | RG_KEY_START},\
+    {RG_KEY_X,      .src = RG_KEY_OPTION | RG_KEY_A},\
+    {RG_KEY_Y,      .src = RG_KEY_OPTION | RG_KEY_B},\
+}
+
+#define RG_RECOVERY_BTN RG_KEY_START
+
+// Battery
+#define RG_BATTERY_DRIVER           1
+#define RG_BATTERY_ADC_UNIT         ADC_UNIT_1
+#define RG_BATTERY_ADC_CHANNEL      ADC_CHANNEL_3
+#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 3150.f) / (4100.f - 3150.f) * 100.f)
+#define RG_BATTERY_CALC_VOLTAGE(raw) ((raw) * 2.f * 0.001f)
+
+// Status LED
+#define RG_GPIO_LED                 GPIO_NUM_6
+
+// SPI Display
+#define RG_GPIO_LCD_MISO            GPIO_NUM_NC
+#define RG_GPIO_LCD_MOSI            GPIO_NUM_11
+#define RG_GPIO_LCD_CLK             GPIO_NUM_12
+#define RG_GPIO_LCD_CS              GPIO_NUM_10
+#define RG_GPIO_LCD_DC              GPIO_NUM_45
+#define RG_GPIO_LCD_BCKL            GPIO_NUM_21
+#define RG_GPIO_LCD_RST             GPIO_NUM_46
+
+// External I2S DAC
+#define RG_GPIO_SND_I2S_BCK         41
+#define RG_GPIO_SND_I2S_WS          42
+#define RG_GPIO_SND_I2S_DATA        01
