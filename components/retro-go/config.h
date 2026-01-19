341687d6e4fe287a57fc61133ac9bcffeb0c7821
Alberto Bonizzi <aseoista@gmail.com>
2026-01-19 13:08:06 +0100

    New Device: REDROID-GO (#246)
    * Add support for redroid-go
    
    * Upddate redroid-go config
    
    * fix: update env.py for redroid-go target
    
    * Fix RG_TARGET_NAME for redroid go target
    
    * Add redroid-go device doc
    
    * Add redroid-go target to README

--- components/retro-go/config.h
+++ components/retro-go/config.h
@@ -30,6 +30,8 @@
 #include "targets/vmu/config.h"
 #elif defined(RG_TARGET_CROKPOCKET)
 #include "targets/crokpocket/config.h"
+#elif defined(RG_TARGET_REDROID_GO)
+#include "targets/redroid-go/config.h"
 #else
 #warning "No target defined. Defaulting to ODROID-GO."
 #include "targets/odroid-go/config.h"
